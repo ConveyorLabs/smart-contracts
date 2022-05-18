9c20b1bb6fe78f5501e5d8dbd2dcd0e8e333d9fe
0xKitsune <0xKitsune@protonmail.com>
2022-05-18 15:05:11 -0400

    updated state structures

--- src/ConveyorLimitOrders.sol
+++ src/ConveyorLimitOrders.sol
@@ -13,7 +13,6 @@ import "../lib/libraries/PriceLibrary.sol";
 import "../lib/libraries/ConveyorMath64x64.sol";
 
 contract ConveyorLimitOrders {
-    
     //----------------------Modifiers------------------------------------//
 
     modifier onlyEOA() {
@@ -26,7 +25,7 @@ contract ConveyorLimitOrders {
     event OrderEvent(
         EventType indexed eventType,
         address indexed sender,
-        Order[] indexed orders
+        bytes32[] indexed orderIds
     );
 
     //----------------------Errors------------------------------------//
@@ -59,8 +58,11 @@ contract ConveyorLimitOrders {
 
     //----------------------Factory/Router Address's------------------------------------//
     /// @dev 0-Uniswap V2 Factory, 1-Uniswap V3 Factory
-    address[] dexFactories = [0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f,0x1F98431c8aD98523631AE4a59f267346ea31F984];
-   
+    //TODO: add logic to initialize this in the contstructor
+    address[] dexFactories = [
+        0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f,
+        0x1F98431c8aD98523631AE4a59f267346ea31F984
+    ];
 
     //----------------------Structs------------------------------------//
 
@@ -71,17 +73,6 @@ contract ConveyorLimitOrders {
         OrderType orderType;
         uint256 price;
         uint256 quantity;
-        bool exists;
-    }
-
-    /// @notice Struct containing mapping(orderId => Order) where 'Order' is the order struct containing the properties of the Order
-    struct OrderGroup {
-        mapping(bytes32 => Order) orders;
-        uint256 totalOrderValue;
-    }
-
-    struct TokenToOrderGroup {
-        mapping(address => OrderGroup) orderGroup;
     }
 
     /// @notice Struct to store important Dex specifications
@@ -93,15 +84,18 @@ contract ConveyorLimitOrders {
 
     //----------------------State Structures------------------------------------//
 
-    /// @notice mapping from mapping(eoaAddress => mapping(token => OrderGroup)) to store the current Active orders in Conveyor state structure
-    mapping(address => TokenToOrderGroup) ActiveOrders;
+    //order id  to order
+    mapping(bytes32 => Order) orderIdToOrder;
+
+    //struct to check if order exists, as well as get all orders for a wallet
+    mapping(address => mapping(bytes32 => bool)) addressToOrderIds;
 
     /// @notice Array of dex structures to be used throughout the contract for pair spot price calculations
     Dex[] public dexes;
 
-     //----------------------Constructor------------------------------------//
+    //----------------------Constructor------------------------------------//
 
-     constructor() {}
+    constructor() {}
 
     //----------------------Functions------------------------------------//
 
@@ -110,19 +104,8 @@ contract ConveyorLimitOrders {
         address token,
         bytes32 orderId
     ) public view returns (Order memory order) {
-        order = ActiveOrders[eoaAddress].orderGroup[token].orders[orderId];
-    }
-
-    /// @notice Add Dex struct to dexes array from arr _factory, and arr _hexDem
-    /// @param _factory address[] dex factory address's to add
-    /// @param _hexDem Factory address create2 deployment bytecode array
-    /// @param isUniV2 Array of bool's indicating uniV2 status
-    function addDex(address[] memory _factory, bytes32[] memory _hexDem, bool[] memory isUniV2) public {
-        require((_factory.length == _hexDem.length && _hexDem.length== isUniV2.length), "Invalid input, Arr length mismatch");
-        for(uint256 i = 0; i < _factory.length; i++){
-            Dex memory d = Dex(_factory[i], _hexDem[i], isUniV2[i]);
-            dexes.push(d);
-        }
+        order = orderIdToOrder[orderId];
+        return order;
     }
 
     /// @notice Add user's order into the Active order's mapping conditionally if the oder passes all of the safety check criterion
@@ -137,10 +120,8 @@ contract ConveyorLimitOrders {
         //token that the orders are being placed on
         address orderToken = orderGroup[0].token;
 
-        //get the current totalOrderValue for all existing orders on the token
-        uint256 totalOrderValue = ActiveOrders[msg.sender]
-            .orderGroup[orderToken]
-            .totalOrderValue;
+        //TODO: sum all orders to check against total order value
+        uint256 totalOrdersValue;
 
         uint256 tokenBalance = IERC20(orderToken).balanceOf(msg.sender);
 
@@ -151,15 +132,14 @@ contract ConveyorLimitOrders {
                 revert IncongruentTokenInOrderGroup();
             }
 
-            //add the order quant to total order value
-            totalOrderValue += newOrder.quantity;
+            totalOrdersValue += newOrder.quantity;
 
             //check if the wallet has a sufficient balance
-            if (tokenBalance < totalOrderValue) {
+            if (tokenBalance < totalOrdersValue) {
                 revert InsufficientWalletBalance();
             }
 
-            //create the new orderId
+            //TODO: create new order id construction that is simpler
             bytes32 orderId = keccak256(
                 abi.encodePacked(
                     msg.sender,
@@ -170,118 +150,96 @@ contract ConveyorLimitOrders {
                 )
             );
 
-            //set exists to true
-            newOrder.exists = true;
+            //TODO: add order to all necessary state
 
-            //add the order to active orders
-            ActiveOrders[msg.sender].orderGroup[orderToken].orders[
-                orderId
-            ] = newOrder;
+            //TODO: add the order to active orders
 
             orderIds[orderIdIndex] = orderId;
             ++orderIdIndex;
         }
 
-        //set the updated total value
-        ActiveOrders[msg.sender]
-            .orderGroup[orderToken]
-            .totalOrderValue = totalOrderValue;
-
         //emit orders placed
-        emit OrderEvent(EventType.PLACE, msg.sender, orderGroup);
+        emit OrderEvent(EventType.PLACE, msg.sender, orderIds);
 
         return orderIds;
     }
 
     /// @notice Update mapping(uint256 => Order) in Order struct from identifier orderId to new 'order' value passed as @param
     function updateOrder(Order calldata newOrder) public {
-        Order memory oldOrder = ActiveOrders[msg.sender]
-            .orderGroup[newOrder.token]
-            .orders[newOrder.orderId];
-
         //check if the old order exists
-        if (!oldOrder.exists) {
+
+        bool orderExists = addressToOrderIds[msg.sender][newOrder.orderId];
+
+        if (!orderExists) {
             revert OrderDoesNotExist(newOrder.orderId);
         }
 
-        uint256 totalOrdersValue = ActiveOrders[msg.sender]
-            .orderGroup[newOrder.token]
-            .totalOrderValue;
+        Order memory oldOrder = orderIdToOrder[newOrder.orderId];
 
-        if (newOrder.quantity > oldOrder.quantity) {
-            totalOrdersValue += newOrder.quantity - oldOrder.quantity;
-        } else {
-            totalOrdersValue += oldOrder.quantity - newOrder.quantity;
-        }
-        //adjust total orders value quanity
+        //TODO: get total order sum and make sure that the user has the balance for the new order
 
-        //check if the wallet has a sufficient balance
-        if (IERC20(newOrder.token).balanceOf(msg.sender) < totalOrdersValue) {
-            revert InsufficientWalletBalance();
-        }
+        // if (newOrder.quantity > oldOrder.quantity) {
+        //     totalOrdersValue += newOrder.quantity - oldOrder.quantity;
+        // } else {
+        //     totalOrdersValue += oldOrder.quantity - newOrder.quantity;
+        // }
 
-        //update totalOrdersValue for that token
-        ActiveOrders[msg.sender]
-            .orderGroup[newOrder.token]
-            .totalOrderValue = totalOrdersValue;
+        // //check if the wallet has a sufficient balance
+        // if (IERC20(newOrder.token).balanceOf(msg.sender) < totalOrdersValue) {
+        //     revert InsufficientWalletBalance();
+        // }
 
         //update the order
-        ActiveOrders[msg.sender].orderGroup[newOrder.token].orders[
-            newOrder.orderId
-        ] = newOrder;
+        orderIdToOrder[oldOrder.orderId] = newOrder;
 
         //emit order updated
         //TODO: still need to decide on contents of events
 
-        Order[] memory orders = new Order[](1);
-        orders[0] = newOrder;
-        emit OrderEvent(EventType.UPDATE, msg.sender, orders);
+        bytes32[] memory orderIds = new bytes32[](1);
+        orderIds[0] = newOrder.orderId;
+        emit OrderEvent(EventType.UPDATE, msg.sender, orderIds);
     }
 
     /// @notice Remove Order order from OrderGroup mapping by identifier orderId conditionally if order exists already in ActiveOrders
-    /// @param token the order to which the caller is removing from the OrderGroup struct
     /// @param orderId the order to which the caller is removing from the OrderGroup struct
-    function cancelOrder(address token, bytes32 orderId) public {
+    function cancelOrder(bytes32 orderId) public {
         /// Check if order exists in active orders. Revert if order does not exist
-        if (
-            !ActiveOrders[msg.sender].orderGroup[token].orders[orderId].exists
-        ) {
+        bool orderExists = addressToOrderIds[msg.sender][orderId];
+
+        if (!orderExists) {
             revert OrderDoesNotExist(orderId);
         }
 
-        Order memory order = ActiveOrders[msg.sender].orderGroup[token].orders[
-            orderId
-        ];
-        /// Get the orderQuantity from the existing order
-        uint256 orderQuantity = order.quantity;
-
-        //update totalOrdersValue to decrease by amount orderQuantity of the order being removed
-        ActiveOrders[msg.sender]
-            .orderGroup[token]
-            .totalOrderValue -= orderQuantity;
+        Order memory order = orderIdToOrder[orderId];
 
         // Delete Order Orders[order.orderId] from ActiveOrders mapping
-        delete ActiveOrders[msg.sender].orderGroup[token].orders[orderId];
+        delete orderIdToOrder[orderId];
+        delete addressToOrderIds[msg.sender][orderId];
 
-        //emit OrderEvent CANCEL
-        Order[] memory orders = new Order[](1);
-        orders[0] = order;
-        emit OrderEvent(EventType.CANCEL, msg.sender, orders);
+        bytes32[] memory orderIds = new bytes32[](1);
+        orderIds[0] = order.orderId;
+        emit OrderEvent(EventType.CANCEL, msg.sender, orderIds);
     }
 
     /// @notice cancel all orders relevant in ActiveOders mapping to the msg.sender i.e the function caller
-    function cancelAllOrders() public {
-        //security checks
+    function cancelOrders(bytes32[] memory orderIds) public {
+        bytes32[] memory canceledOrderIds = new bytes32[](orderIds.length);
 
         //check that there is one or more orders
+        for (uint256 i = 0; i < orderIds.length; ++i) {
+            bytes32 orderId = orderIds[i];
+            bool orderExists = addressToOrderIds[msg.sender][orderId];
 
-        //get all orders for the eoa first, this is pseudo code atm
-        delete ActiveOrders[msg.sender];
+            if (!orderExists) {
+                revert OrderDoesNotExist(orderId);
+            }
 
-        //emit all order cancel
-        //pass in an empty list to signify that all orders have been canceled
-        Order[] memory orders;
-        emit OrderEvent(EventType.PLACE, msg.sender, orders);
+            delete addressToOrderIds[msg.sender][orderId];
+            delete orderIdToOrder[orderId];
+            canceledOrderIds[i] = orderId;
+        }
+
+        emit OrderEvent(EventType.PLACE, msg.sender, canceledOrderIds);
     }
 
     function swapAndPlaceOrders() public {}
@@ -318,27 +276,47 @@ contract ConveyorLimitOrders {
     /// @param token0 bytes32 address of token0
     /// @param token1 bytes32 address of token1
     /// @return uint256 spot price of token1 with respect to token2 i.e reserve1/reserve2
-    function calculateMeanPairSpotPrice(address token0, address token1) external view returns (uint256) {
-        return PriceLibrary.calculateMeanSpotPrice(token0, token1, dexes,1, 3000);
+    function calculateMeanPairSpotPrice(address token0, address token1)
+        external
+        view
+        returns (uint256)
+    {
+        return
+            PriceLibrary.calculateMeanSpotPrice(token0, token1, dexes, 1, 3000);
     }
 
     /// @notice Helper function to get Uniswap V2 spot price of pair token1/token2
     /// @param token0 bytes32 address of token1
     /// @param token1 bytes32 address of token2
     /// @return uint256 spot price of token1 with respect to token2 i.e reserve1/reserve2
-    function calculateMinPairSpotPrice(address token0, address token1) external view returns (uint256) {
-        return PriceLibrary.calculateMinSpotPrice(token0, token1, dexes,1, 3000);
+    function calculateMinPairSpotPrice(address token0, address token1)
+        external
+        view
+        returns (uint256)
+    {
+        return
+            PriceLibrary.calculateMinSpotPrice(token0, token1, dexes, 1, 3000);
     }
 
     /// @notice Helper function to calculate the logistic mapping output on a USDC input quantity for fee % calculation
     /// @dev calculation assumes 64x64 fixed point in128 representation for all values
     /// @param amountIn uint128 USDC amount in 64x64 fixed point to calculate the fee % of
     /// @return Out64x64 int128 Fee percent
-    function calculateFee(int128 amountIn) public pure returns (int128 Out64x64) {
-        require(!(amountIn << 64 > 0xfffffffffffffffffffffffffff), "Overflow Error");
+    function calculateFee(int128 amountIn)
+        public
+        pure
+        returns (int128 Out64x64)
+    {
+        require(
+            !(amountIn << 64 > 0xfffffffffffffffffffffffffff),
+            "Overflow Error"
+        );
         int128 iamountIn = amountIn << 64;
         int128 numerator = 16602069666338597000; //.9 sccale := 1e19 ==> 64x64 fixed representation
-        int128 denominator = (23058430092136940000+ConveyorMath64x64.exp(ConveyorMath64x64.div(iamountIn, 75000 << 64)));
+        int128 denominator = (23058430092136940000 +
+            ConveyorMath64x64.exp(
+                ConveyorMath64x64.div(iamountIn, 75000 << 64)
+            ));
         int128 rationalFraction = ConveyorMath64x64.div(numerator, denominator);
         Out64x64 = rationalFraction + 1844674407370955300;
     }
@@ -348,7 +326,11 @@ contract ConveyorLimitOrders {
     /// @param wethValue uint256 total order value in wei at execution price
     /// @return conveyorReward conveyor reward in terms of wei
     /// @return beaconReward beacon reward in wei
-    function calculateReward(int128 percentFee, int128 wethValue) public pure returns (int128 conveyorReward, int128 beaconReward){
+    function calculateReward(int128 percentFee, int128 wethValue)
+        public
+        pure
+        returns (int128 conveyorReward, int128 beaconReward)
+    {
         /// Todo calculate the beaconReward/conveyorReward based on applying percentFee to wethValue
     }
 
@@ -356,7 +338,11 @@ contract ConveyorLimitOrders {
     /// @param orderGroup := array of order's to be placed
     /// @param gasPrice uint256 in gwei
     /// @return bool := boolean value indicating whether gas credit's provide coverage over all orders in the orderGroup
-    function hasMinGasCredits(Order[] calldata orderGroup, uint256 gasPrice) internal pure returns (bool) {
+    function hasMinGasCredits(Order[] calldata orderGroup, uint256 gasPrice)
+        internal
+        pure
+        returns (bool)
+    {
         /// Todo iterate through each order in orderGroup, check if gas credits is satisfied for each order
     }
 
@@ -364,7 +350,11 @@ contract ConveyorLimitOrders {
     /// @param order Order struct to be checked for minimum gas credits
     /// @param gasPrice uint256 in gwei
     /// @return minCredits uint256 minimum gas credits required represented in wei
-    function calculateMinGasCredits(Order calldata order, uint256 gasPrice) internal pure returns (uint256) {
+    function calculateMinGasCredits(Order calldata order, uint256 gasPrice)
+        internal
+        pure
+        returns (uint256)
+    {
         /// Todo determine the execution cost based on gasPrice, and return minimum gasCredits required in wei for order placement
     }
 
@@ -372,9 +362,36 @@ contract ConveyorLimitOrders {
     /// @param snapShotSpot uint256 snapShotSpot of the lowest execution spot price of the whole batch
     /// @param reserve0 uint256 reserve0 of lp at execution time
     /// @param reserve1 uint256 reserve1 of lp at execution time
-    /// @param fee uint256 lp fee 
+    /// @param fee uint256 lp fee
     /// @return maxBeaconReward uint256 maximum safe beacon reward to protect against flash loan price manipulation in the lp
-    function calculateMaxBeaconReward(uint256 snapShotSpot, uint256 reserve0, uint256 reserve1, uint8 fee) internal pure returns (uint256) {
+    function calculateMaxBeaconReward(
+        uint256 snapShotSpot,
+        uint256 reserve0,
+        uint256 reserve1,
+        uint8 fee
+    ) internal pure returns (uint256) {
         /// Todo calulate alphaX and multiply by fee to determine max beacon reward quantity
-    } 
+    }
+
+    //------------------------Admin Functions----------------------------
+
+    /// @notice Add Dex struct to dexes array from arr _factory, and arr _hexDem
+    /// @param _factory address[] dex factory address's to add
+    /// @param _hexDem Factory address create2 deployment bytecode array
+    /// @param isUniV2 Array of bool's indicating uniV2 status
+    function addDex(
+        address[] memory _factory,
+        bytes32[] memory _hexDem,
+        bool[] memory isUniV2
+    ) public {
+        require(
+            (_factory.length == _hexDem.length &&
+                _hexDem.length == isUniV2.length),
+            "Invalid input, Arr length mismatch"
+        );
+        for (uint256 i = 0; i < _factory.length; i++) {
+            Dex memory d = Dex(_factory[i], _hexDem[i], isUniV2[i]);
+            dexes.push(d);
+        }
+    }
 }
