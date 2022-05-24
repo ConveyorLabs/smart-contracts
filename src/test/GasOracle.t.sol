ce19d56bf4b986dac6a326ae32c2bf0eec8d3442
0xKitsune <0xKitsune@protonmail.com>
2022-05-24 13:18:09 -0400

    updated test architecture

---
+++ src/test/GasOracle.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.13;
+
+import "./utils/test.sol";
+import "./utils/Console.sol";
+import "./utils/Utils.sol";
+
+import "../ConveyorLimitOrders.sol";
+import "../../lib/interfaces/uniswap-v2/IUniswapV2Router02.sol";
+import "../../lib/interfaces/uniswap-v2/IUniswapV2Factory.sol";
+import "../../lib/interfaces/token/IERC20.sol";
+import "../../lib/libraries/PriceLibrary.sol";
+
+interface CheatCodes {
+    function prank(address) external;
+
+    function deal(address who, uint256 amount) external;
+}
+
+contract GasOracleTest is DSTest {}
+
+contract GasOracleWrapper is GasOracle {
+    constructor(address _gasOracle) GasOracle(_gasOracle) {}
+}
