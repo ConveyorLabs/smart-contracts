8398cbedcc1aa4a46e7ba58c0860dd3168598f15
0xKitsune <0xKitsune@protonmail.com>
2022-06-22 09:38:47 -0400

    updated script runner

--- src/test/utils/python_deployer/IAlphaX.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.13;
-
-interface IAlphaX {
-
-    function alphaX(uint128 reserve0Snapshot, uint128 reserve1Snapshot, uint128 reserve0Execution, uint128 reserve1Execution) external returns (uint256);
-}
