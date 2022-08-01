3fa5b32306d1d1c3897950ffa957ccf603925ac2
0xKitsune <0xKitsune@protonmail.com>
2022-08-01 16:21:49 -0400

    Updated gas oracle, needs stale price check, updated orderbook adding comments.

--- src/GasOracle.sol
+++ src/GasOracle.sol
@@ -1,15 +1,21 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
 import "../lib/AggregatorV3Interface.sol";
 
+/// @title GasOracle
+/// @author LeytonTaylor, 0xKitsune
+/// @notice This contract fetches the latest fast gas price from the Chainlink Gas Oracle
 contract GasOracle {
+    ///@notice The gasOracleAddress is the address of the Chainlink Gas Oracle
     address immutable gasOracleAddress;
 
     constructor(address _gasOracleAddress) {
         gasOracleAddress = _gasOracleAddress;
     }
 
+    ///@notice Gets the latest gas price from the Chainlink data feed for the fast gas oracle
+    //TODO: FIXME: stale gas result check
     function getGasPrice() public view returns (uint256) {
         (, int256 answer, , , ) = IAggregatorV3(gasOracleAddress)
             .latestRoundData();
--- src/OrderBook.sol
+++ src/OrderBook.sol
@@ -1,10 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.14;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
 import "../lib/interfaces/token/IERC20.sol";
 import "./GasOracle.sol";
 import "./ConveyorErrors.sol";
-import "./test/utils/Console.sol";
 
 contract OrderBook is GasOracle {
     //----------------------Constructor------------------------------------//
@@ -12,18 +11,42 @@ contract OrderBook is GasOracle {
     constructor(address _gasOracle) GasOracle(_gasOracle) {}
 
     //----------------------Events------------------------------------//
-
+    /**@notice Event that is emitted when a new order is placed. For each order that is placed, the corresponding orderId is added
+    to the orderIds param. 
+     */
     event OrderPlaced(bytes32[] indexed orderIds);
+
+    /**@notice Event that is emitted when an order is cancelled. For each order that is cancelled, the corresponding orderId is added
+    to the orderIds param. 
+     */
     event OrderCancelled(bytes32[] indexed orderIds);
+
+    /**@notice Event that is emitted when a new order is update. For each order that is updated, the corresponding orderId is added
+    to the orderIds param. 
+     */
     event OrderUpdated(bytes32[] indexed orderIds);
 
-    //TODO: Order Filled event, emit this for all the order ids that are filled
+    /**@notice Event that is emitted when an order is filled. For each order that is filled, the corresponding orderId is added
+    to the orderIds param. 
+     */
     event OrderFilled(bytes32[] indexed orderIds);
 
     //----------------------Structs------------------------------------//
 
-    /// @notice Struct containing the token, orderId, OrderType enum type, price, and quantity for each order
-    /// @notice assuming slippage set to 128.128 fixed point representation
+    ///@notice Struct containing Order details for any limit order
+    ///@param buy - Indicates if the order is a buy or sell
+    ///@param taxed - Indicates if the tokenIn or tokenOut is taxed. This will be set to true if one or both tokens are taxed.
+    ///@param lastRefreshTimestamp - Unix timestamp representing the last time the order was refreshed.
+    ///@param expirationTimestamp - Unix timestamp representing when the order should expire.
+    ///@param feeIn - The Univ3 liquidity pool fee for the tokenIn/Weth pairing.
+    ///@param feeOut - The Univ3 liquidity pool fee for the tokenOut/Weth pairing.
+    ///@param price - The execution price representing the spot price of tokenIn/tokenOut that the order should be filled at. This is represented as a 64x64 fixed point number.
+    ///@param amountOutMin - The minimum amount out that the order owner is willing to accept. This value is represented in tokenOut.
+    ///@param quantity - The amount of tokenIn that the order use as the amountIn value for the swap (represented in amount * 10**tokenInDecimals).
+    ///@param owner - The owner of the order. This is set to the msg.sender at order placement.
+    ///@param tokenIn - The tokenIn for the order.
+    ///@param tokenOut - The tokenOut for the order.
+    ///@param orderId - Unique identifier for the order.
     struct Order {
         bool buy;
         bool taxed;
@@ -43,24 +66,26 @@ contract OrderBook is GasOracle {
 
     //----------------------State Structures------------------------------------//
 
-    //order id  to order
-    //TODO: deleting doesnt actually get rid of the order, just removes the pointer
+    ///@notice Mapping from an orderId to its order.
     mapping(bytes32 => Order) public orderIdToOrder;
 
-    //keccak256(msg.sender, tokenAddress) -> total orders quantity
+    ///@notice Mapping to find the total orders quantity for a specific token, for an individual account
+    ///@notice The key is represented as: keccak256(abi.encode(owner, token));
     mapping(bytes32 => uint256) public totalOrdersQuantity;
 
-    //struct to check if order exists, as well as get all orders for a wallet
+    ///@notice Mapping to check if an order exists, as well as get all the orders for an individual account.
+    ///@dev ownerAddress -> orderId -> bool
     mapping(address => mapping(bytes32 => bool)) public addressToOrderIds;
 
-    //Mapping to get total order count for a users address
+    ///@notice Mapping to store the number of total orders for an individual account
     mapping(address => uint256) public totalOrdersPerAddress;
 
-    //unique number that is used to hash order ids
+    ///@notice The orderNonce is a unique value is used to create orderIds and increments every time a new order is placed.
     uint256 orderNonce;
 
     //----------------------Functions------------------------------------//
 
+    ///@notice This function gets an order by the orderId. If the order does not exist, the order returned will be empty.
     function getOrderById(bytes32 orderId)
         public
         view
@@ -70,79 +95,106 @@ contract OrderBook is GasOracle {
         return order;
     }
 
-    /// @notice Add user's order into the Active order's mapping conditionally if the oder passes all of the safety check criterion
-    /// @param orderGroup := array of orders to be added to ActiveOrders mapping in OrderGroup struct
-    /// @return orderIds
+    ///@notice Places a new order (or group of orders) into the system.
+    ///@param orderGroup - List of newly created orders to be placed.
+    /// @return orderIds - Returns a list of orderIds corresponding to the newly placed orders.
     function placeOrder(Order[] calldata orderGroup)
         public
         returns (bytes32[] memory)
     {
+        ///@notice Value responsible for keeping track of array indices when placing a group of new orders
         uint256 orderIdIndex;
 
+        ///@notice Initialize a new list of bytes32 to store the newly created orderIds.
         bytes32[] memory orderIds = new bytes32[](orderGroup.length);
-        //token that the orders are being placed on
+
+        ///@notice Initialize the orderToken for the newly placed orders.
+        /**@dev When placing a new group of orders, the tokenIn and tokenOut must be the same on each order. New orders are placed
+        this way to securely validate if the msg.sender has the tokens required when placing a new order as well as enough gas credits
+        to cover order execution cost.
+        */
         address orderToken = orderGroup[0].tokenIn;
 
-        uint256 totalOrdersValue = _getTotalOrdersValue(orderToken);
+        ///@notice Get the value of all orders on the orderToken that are currently placed for the msg.sender.
+        uint256 updatedTotalOrdersValue = _getTotalOrdersValue(orderToken);
+
+        ///@notice Get the current balance of the orderToken that the msg.sender has in their account.
         uint256 tokenBalance = IERC20(orderToken).balanceOf(msg.sender);
 
-        for (uint256 i = 0; i < orderGroup.length; ++i) {
+        ///@notice For each order within the list of orders passed into the function.
+        for (uint256 i = 0; i < orderGroup.length; ) {
+            ///@notice Get the order details from the orderGroup.
             Order memory newOrder = orderGroup[i];
 
-            totalOrdersValue += newOrder.quantity;
+            ///@notice Increment the total value of orders by the quantity of the new order
+            updatedTotalOrdersValue += newOrder.quantity;
 
+            ///@notice If the newOrder's tokenIn does not match the orderToken, revert.
             if (!(orderToken == newOrder.tokenIn)) {
                 revert IncongruentTokenInOrderGroup();
             }
 
-            //check if the wallet has a sufficient balance
-            if (tokenBalance < totalOrdersValue) {
+            ///@notice If the msg.sender does not have a sufficent balance to cover the order, revert.
+            if (tokenBalance < updatedTotalOrdersValue) {
                 revert InsufficientWalletBalance();
             }
 
-            //increment total orders Quantity
-            incrementTotalOrdersQuantity(
-                orderToken,
-                msg.sender,
-                newOrder.quantity
-            );
-
+            ///@notice Create a new orderId from the orderNonce and current block timestamp
             bytes32 orderId = keccak256(
                 abi.encode(orderNonce, block.timestamp)
             );
 
-            //increment order nonce
+            ///@notice increment the orderNonce
+            /**@dev This is unchecked because the orderNonce and block.timestamp will never be the same, so even if the 
+            orderNonce overflows, it will still produce unique orderIds because the timestamp will be different.
+            */
             unchecked {
                 ++orderNonce;
             }
 
-            //set the order owner as the msg.sender
+            ///@notice Set the new order's owner to the msg.sender
             newOrder.owner = msg.sender;
 
-            //add new order to state
+            ///@notice update the newOrder's Id to the orderId generated from the orderNonce
             newOrder.orderId = orderId;
+
+            ///@notice Add the newly created order to the orderIdToOrder mapping
             orderIdToOrder[orderId] = newOrder;
 
+            ///@notice Add the orderId to the addressToOrderIds mapping
             addressToOrderIds[msg.sender][orderId] = true;
-            //update total orders per address
 
+            ///@notice Increment the total orders per address for the msg.sender
             ++totalOrdersPerAddress[msg.sender];
 
-            //update order ids for event emission
+            ///@notice Add the orderId to the orderIds array for the PlaceOrder event emission and increment the orderIdIndex
             orderIds[orderIdIndex] = orderId;
             ++orderIdIndex;
+
+            unchecked {
+                ++i;
+            }
         }
 
+        ///@notice Update the total orders value on the orderToken for the msg.sender.
+        updateTotalOrdersQuantity(
+            orderToken,
+            msg.sender,
+            updatedTotalOrdersValue
+        );
+
+        ///@notice Get the total amount approved for the ConveyorLimitOrder contract to spend on the orderToken.
         uint256 totalApprovedQuantity = IERC20(orderToken).allowance(
             msg.sender,
             address(this)
         );
 
-        if (totalApprovedQuantity < _getTotalOrdersValue(orderToken)) {
+        ///@notice If the total approved quantity is less than the updatedTotalOrdersValue, revert.
+        if (totalApprovedQuantity < updatedTotalOrdersValue) {
             revert InsufficientAllowanceForOrderPlacement();
         }
 
-        //emit orders placed
+        ///@notice Emit an OrderPlaced event to notify the off-chain executors that a new order has been placed.
         emit OrderPlaced(orderIds);
 
         return orderIds;
@@ -199,7 +251,6 @@ contract OrderBook is GasOracle {
         }
 
         Order memory oldOrder = orderIdToOrder[newOrder.orderId];
-
 
         ///TODO: make this more efficient and check if new order > old order, then increment the difference else decrement the difference
 
@@ -328,6 +379,15 @@ contract OrderBook is GasOracle {
         bytes32 totalOrdersValueKey = keccak256(abi.encode(owner, token));
 
         totalOrdersQuantity[totalOrdersValueKey] += quantity;
+    }
+
+    function updateTotalOrdersQuantity(
+        address token,
+        address owner,
+        uint256 newQuantity
+    ) internal {
+        bytes32 totalOrdersValueKey = keccak256(abi.encode(owner, token));
+        totalOrdersQuantity[totalOrdersValueKey] = newQuantity;
     }
 
     /// @notice Internal helper function to approximate the minimum gas credits for a user assuming all Order's are standard erc20 compliant
