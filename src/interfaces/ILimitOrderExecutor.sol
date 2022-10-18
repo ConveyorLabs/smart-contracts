1f1b58057b3dd9f9fe45f4bb606e62520d6b4652
0xKitsune <0xKitsune@protonmail.com>
2022-10-18 14:55:20 -0400

    updated executor interface

--- src/interfaces/ILimitOrderExecutor.sol
+++ src/interfaces/ILimitOrderExecutor.sol
@@ -1,50 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import "../SwapRouter.sol";
+import "../OrderBook.sol";
 
 interface ILimitOrderExecutor {
-    function _findBestTokenToTokenExecutionPrice(
-        SwapRouter.TokenToTokenExecutionPrice[] memory executionPrices,
-        bool buyOrder
-    ) external pure returns (uint256 bestPriceIndex);
-
-    function simulateTokenToTokenPriceChange(
-        uint128 alphaX,
-        SwapRouter.TokenToTokenExecutionPrice memory executionPrice
-    ) external returns (SwapRouter.TokenToTokenExecutionPrice memory);
-
-    function simulateTokenToWethPriceChange(
-        uint128 alphaX,
-        SwapRouter.TokenToWethExecutionPrice memory executionPrice
-    ) external returns (SwapRouter.TokenToWethExecutionPrice memory);
-
-    function _findBestTokenToWethExecutionPrice(
-        SwapRouter.TokenToWethExecutionPrice[] memory executionPrices,
-        bool buyOrder
-    ) external pure returns (uint256 bestPriceIndex);
-
-    function calculateAmountOutMinAToWeth(
-        address lpAddressAToWeth,
-        uint256 amountInOrder,
-        uint16 taxIn,
-        uint24 feeIn,
-        address tokenIn
-    ) external returns (uint256 amountOutMinAToWeth);
-
-    function _initializeTokenToWethExecutionPrices(
-        SwapRouter.SpotReserve[] memory spotReserveAToWeth,
-        address[] memory lpAddressesAToWeth
-    ) external view returns (SwapRouter.TokenToWethExecutionPrice[] memory);
-
-    function _initializeTokenToTokenExecutionPrices(
-        address tokenIn,
-        SwapRouter.SpotReserve[] memory spotReserveAToWeth,
-        address[] memory lpAddressesAToWeth,
-        SwapRouter.SpotReserve[] memory spotReserveWethToB,
-        address[] memory lpAddressWethToB
-    ) external view returns (SwapRouter.TokenToTokenExecutionPrice[] memory);
-
     function executeTokenToWethOrders(OrderBook.Order[] memory orders)
         external
         returns (uint256, uint256);
