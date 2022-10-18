6920675cb16668de7dbde5b2d8f436200bfbe91c
0xKitsune <0xKitsune@protonmail.com>
2022-10-18 16:21:46 -0400

    multiplied gas price when calculating execution gas consumed

--- src/LimitOrderRouter.sol
+++ src/LimitOrderRouter.sol
@@ -200,7 +200,7 @@ contract LimitOrderRouter is OrderBook {
             ///@notice Check that the account has enough gas credits to refresh the order, otherwise, cancel the order and continue the loop.
             if (gasCreditBalance[order.owner] < REFRESH_FEE) {
                 _cancelOrder(order);
-                
+
                 unchecked {
                     ++i;
                 }
@@ -399,7 +399,11 @@ contract LimitOrderRouter is OrderBook {
 
     ///@notice This function is called by off-chain executors, passing in an array of orderIds to execute a specific batch of orders.
     /// @param orderIds - Array of orderIds to indicate which orders should be executed.
-    function executeOrders(bytes32[] calldata orderIds) external onlyEOA nonReentrant {
+    function executeOrders(bytes32[] calldata orderIds)
+        external
+        onlyEOA
+        nonReentrant
+    {
         //Update the initial gas balance.
         assembly {
             sstore(initialTxGas.slot, gas())
@@ -505,8 +509,12 @@ contract LimitOrderRouter is OrderBook {
         view
         returns (uint256 executionGasConsumed)
     {
+        uint256 gasPrice = getGasPrice();
         assembly {
-            executionGasConsumed := sub(sload(initialTxGas.slot), gas())
+            executionGasConsumed := mul(
+                gasPrice,
+                sub(sload(initialTxGas.slot), gas())
+            )
         }
     }
 
