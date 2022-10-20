d88d54139c8a5cd8a5376d11ec5ce72eb8324d5c
0xKitsune <0xKitsune@protonmail.com>
2022-10-20 17:59:46 -0400

    updated adjusted gas oracle price

--- src/GasOracle.sol
+++ src/GasOracle.sol
@@ -10,7 +10,7 @@ contract GasOracle {
     ///@notice The gasOracleAddress is the address of the Chainlink Gas Oracle.
     address immutable gasOracleAddress;
 
-    ///@notice Stale Price delay interval between blocks. 
+    ///@notice Stale Price delay interval between blocks.
     constructor(address _gasOracleAddress) {
         gasOracleAddress = _gasOracleAddress;
     }
@@ -20,6 +20,17 @@ contract GasOracle {
         (, int256 answer, , , ) = IAggregatorV3(gasOracleAddress)
             .latestRoundData();
 
-        return uint256(answer);
+        ///@notice
+        /* The gas price is determined to be the oracleGasPrice * 1.25 since the Chainlink Gas Oracle can deviate up to 25% between updates
+         If the Chainlink Gas oracle is reporting a price that is 25% less than the actual price, then the adjustedGasPrice will report the fair market gas price
+         If the Gas oracle is reporting a price 25% greater than the actual gas price, the adjusted price is still 25% greater than the oracle.
+         This allows for the off chain executor to always be incentivized to execute a transaction, regardless of how far the gasOracle deviates
+         from the fair market price. 
+        */
+        uint256 oracleGasPrice = uint256(answer);
+        uint256 adjustedGasPrice = oracleGasPrice +
+            ((oracleGasPrice * 25) / 100);
+
+        return adjustedGasPrice;
     }
 }
