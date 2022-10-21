6c05a4f5c310e7404a839510320a8cad58842799
LeytonTaylor <leytontaylor23@gmail.com>
2022-10-20 19:41:50 -0500

    ConveyorTickMath tests

--- src/test/ConveyotTickMath.t.sol
+++ src/test/ConveyotTickMath.t.sol
@@ -28,30 +28,23 @@ interface CheatCodes {
 }
 
 contract ConveyorTickMathTest is DSTest {
+    //Initialize test contract instances
     ConveyorTickMathWrapper conveyorTickMath;
-    LimitOrderExecutorWrapper limitOrderExecutor;
-
+    SwapRouterWrapper swapRouter;
     IQuoter iQuoter;
-    ScriptRunner scriptRunner;
-
+    //Swap helper
     Swap swapHelper;
-    Swap swapHelperUniV2;
-
-    address uniV2Addr = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-
     //Initialize cheatcodes
     CheatCodes cheatCodes;
 
     //Test Token Address's
     address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address LINK = 0x218532a12a389a4a92fC0C5Fb22901D1c19198aA;
-    address UNI = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
     address USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address TAXED_TOKEN = 0xE7eaec9Bca79d537539C00C58Ae93117fB7280b9; //
-    address TAXED_TOKEN_1 = 0xe0a189C975e4928222978A74517442239a0b86ff; //
-    address TAXED_TOKEN_2 = 0xd99793A840cB0606456916d1CF5eA199ED93Bf97; //6% tax CHAOS token 27
-    address TAXED_TOKEN_3 = 0xcFEB09C3c5F0f78aD72166D55f9e6E9A60e96eEC;
+
+    //Test Pool addresses
+    address daiWethPoolV3 = 0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8;
+    address usdcWethPoolV3 = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
 
     //MAX_UINT for testing
     uint256 constant MAX_UINT = 2**256 - 1;
@@ -65,8 +58,6 @@ contract ConveyorTickMathTest is DSTest {
     // address _sushiFactoryAddress = 0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac;
     address _uniV3FactoryAddress = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
 
-    //Chainlink ERC20 address
-    address swapToken = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
     // bytes32 _sushiHexDem =
     //     hex"e18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303";
     bytes32 _uniswapV2HexDem =
@@ -88,101 +79,61 @@ contract ConveyorTickMathTest is DSTest {
         //  true,
         false
     ];
-
-    uint256 alphaXDivergenceThreshold = 3402823669209385000000000000000000; //0.00001
-    address swapRouter = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
-    address aggregatorV3Address = 0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C;
+    address _uniV2Address = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
     function setUp() public {
-        scriptRunner = new ScriptRunner();
+        swapHelper = new Swap(_uniV2Address, WETH);
+        //Initalize cheatCodes
         cheatCodes = CheatCodes(HEVM_ADDRESS);
-        swapHelper = new Swap(_sushiSwapRouterAddress, WETH);
-        swapHelperUniV2 = new Swap(uniV2Addr, WETH);
+        //Iniialize ConveyorTickMath contract wrapper
         conveyorTickMath = new ConveyorTickMathWrapper();
+        //Initialize the v3 quoter
         iQuoter = IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
-        limitOrderExecutor = new LimitOrderExecutorWrapper(
-            _hexDems,
-            _dexFactories,
-            _isUniV2
-        );
+        //Initialize the Sw
+        swapRouter = new SwapRouterWrapper(_hexDems, _dexFactories, _isUniV2);
     }
 
-    function testsimulateAmountOutOnSqrtPriceX96__ZeroForOneTrue(uint64 _alphaX)
-        public
-    {
-        bool run = true;
-        if (_alphaX == 0) {
-            run = false;
-        }
-
-        if (run) {
-            address poolAddress = 0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8;
-            address tokenIn = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-
-            (uint160 sqrtPriceX96, , , , , , ) = IUniswapV3Pool(poolAddress)
-                .slot0();
-            int24 tickSpacing = IUniswapV3Pool(poolAddress).tickSpacing();
-            address token0 = IUniswapV3Pool(poolAddress).token0();
-
-            uint128 liquidity = IUniswapV3Pool(poolAddress).liquidity();
-            uint160 sqrtPriceLimitX96 = SqrtPriceMath.getNextSqrtPriceFromInput(
-                sqrtPriceX96,
-                liquidity,
-                _alphaX,
-                true
-            );
-            uint256 amountOut = iQuoter.quoteExactInputSingle(
-                tokenIn,
-                WETH,
-                3000,
-                _alphaX,
-                sqrtPriceLimitX96
-            );
-
-            uint256 amountOutMin = uint256(
-                -conveyorTickMath._simulateAmountOutOnSqrtPriceX96(
-                    token0,
-                    tokenIn,
-                    poolAddress,
-                    _alphaX,
-                    tickSpacing,
-                    liquidity,
-                    3000
-                )
-            );
-
-            assertEq(amountOutMin, amountOut);
-        }
-    }
-
-    function testsimulateAmountOutOnSqrtPriceX96__ZeroForOneFalse(
-        uint64 _alphaX
+    ///@notice Test simulateAmountOutOnSqrtPriceX96 Quoted Amount out calculation.
+    ///@dev This tests the case when swapping token0 for token1 in the v3 pool.
+    function testSimulateAmountOutOnSqrtPriceX96__ZeroForOneFalse(
+        uint112 _alphaX
     ) public {
         bool run = true;
-        if (_alphaX == 0) {
+        //range 10-10000 dai
+        if (_alphaX < 1000000000000000000 || _alphaX > 100000000000000000000) {
             run = false;
         }
 
         if (run) {
-            address poolAddress = 0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8;
-            address tokenOut = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-
-            (uint160 sqrtPriceX96, , , , , , ) = IUniswapV3Pool(poolAddress)
+            //Grab all relevant storage data from the v3 pool
+            (uint160 sqrtPriceX96, , , , , , ) = IUniswapV3Pool(daiWethPoolV3)
                 .slot0();
-            int24 tickSpacing = IUniswapV3Pool(poolAddress).tickSpacing();
-            address token0 = IUniswapV3Pool(poolAddress).token0();
-            uint128 liquidity = IUniswapV3Pool(poolAddress).liquidity();
+            int24 tickSpacing = IUniswapV3Pool(daiWethPoolV3).tickSpacing();
+            address token0 = IUniswapV3Pool(daiWethPoolV3).token0();
+            uint128 liquidity = IUniswapV3Pool(daiWethPoolV3).liquidity();
+
+            //Calculate the change in price on the input quantity
             uint160 sqrtPriceLimitX96 = SqrtPriceMath.getNextSqrtPriceFromInput(
                 sqrtPriceX96,
                 liquidity,
                 _alphaX,
                 false
             );
-            uint256 amountOutMin = uint256(
+            //Get the expected amountOut in Dai from the v3 quoter.
+            uint256 amountOutExpected = iQuoter.quoteExactInputSingle(
+                WETH,
+                DAI,
+                3000,
+                _alphaX,
+                sqrtPriceLimitX96
+            );
+
+            //Get the quoted amount out from _simulateAmountOutOnSqrtPriceX96.
+            uint256 amountOutToValidate = uint256(
                 -conveyorTickMath._simulateAmountOutOnSqrtPriceX96(
                     token0,
                     WETH,
-                    poolAddress,
+                    daiWethPoolV3,
                     _alphaX,
                     tickSpacing,
                     liquidity,
@@ -190,95 +141,223 @@ contract ConveyorTickMathTest is DSTest {
                 )
             );
 
-            uint256 amountOut = iQuoter.quoteExactInputSingle(
+            {
+                cheatCodes.deal(address(this), MAX_UINT);
+
+                //Wrap the Ether
+                address(WETH).call{value: 500000000000 ether}(
+                    abi.encodeWithSignature("deposit()")
+                );
+
+                //Transfer the input amount to the SwapRotuer contract to be sent to the pool in the swap callback.
+                IERC20(WETH).transfer(address(swapRouter), _alphaX);
+                
+                //Attempt a swap on our derived quote
+                //Perform the swap
+                uint256 amountReceived = swapRouter.swapV3(
+                    daiWethPoolV3,
+                    WETH,
+                    DAI,
+                    3000,
+                    _alphaX,
+                    amountOutToValidate,
+                    address(this),
+                    address(swapRouter)
+                );
+
+                //Make sure we got more than our amountOutMin
+                assertGe(amountReceived, amountOutToValidate);
+                //Ensure they are equal within 10 wei
+                assertEq(
+                    amountOutToValidate / 10,
+                    amountOutExpected / 10
+                );
+            }
+        }
+    }
+
+    ///@notice Test simulateAmountOutOnSqrtPriceX96 Quoted Amount out calculation.
+    ///@dev This tests the case when swapping token1 for token0 in the v3 pool.
+    function testSimulateAmountOutOnSqrtPriceX96__ZeroForOneTrue(
+        uint112 _alphaX
+    ) public {
+        bool run = true;
+        //range 10-10000 dai
+        if (
+            _alphaX < 10000000000000000000 || _alphaX > 10000000000000000000000
+        ) {
+            run = false;
+        }
+
+        if (run) {
+            //Grab all relevant storage data from the v3 pool
+            (uint160 sqrtPriceX96, , , , , , ) = IUniswapV3Pool(daiWethPoolV3)
+                .slot0();
+            int24 tickSpacing = IUniswapV3Pool(daiWethPoolV3).tickSpacing();
+            address token0 = IUniswapV3Pool(daiWethPoolV3).token0();
+            uint128 liquidity = IUniswapV3Pool(daiWethPoolV3).liquidity();
+
+            //Calculate the change in price on the input quantity
+            uint160 sqrtPriceLimitX96 = SqrtPriceMath.getNextSqrtPriceFromInput(
+                sqrtPriceX96,
+                liquidity,
+                _alphaX,
+                true
+            );
+            //Get the expected amountOut in Dai from the v3 quoter.
+            uint256 amountOutExpected = iQuoter.quoteExactInputSingle(
+                DAI,
                 WETH,
-                tokenOut,
                 3000,
                 _alphaX,
                 sqrtPriceLimitX96
             );
 
-            
+            //Get the quoted amount out from _simulateAmountOutOnSqrtPriceX96.
+            uint256 amountOutToValidate = uint256(
+                -conveyorTickMath._simulateAmountOutOnSqrtPriceX96(
+                    token0,
+                    DAI,
+                    daiWethPoolV3,
+                    _alphaX,
+                    tickSpacing,
+                    liquidity,
+                    3000
+                )
+            );
 
-            assertEq(amountOut, amountOutMin);
+            {
+                cheatCodes.deal(address(swapHelper), MAX_UINT);
+                swapHelper.swapEthForTokenWithUniV2(
+                    100000000000000000000000,
+                    DAI
+                );
+                IERC20(DAI).transfer(address(swapRouter), _alphaX);
+                //Attempt a swap on our derived quote
+                //Perform the swap on the pool to recalculate if the current tick has been crossed
+                uint256 amountReceived = swapRouter.swapV3(
+                    daiWethPoolV3,
+                    DAI,
+                    WETH,
+                    3000,
+                    _alphaX,
+                    amountOutToValidate,
+                    address(this),
+                    address(swapRouter)
+                );
+
+                assertGe(amountReceived, amountOutToValidate);
+                //Ensure they are equal within 10 wei
+                assertEq(
+                    amountOutToValidate/10,
+                    amountOutExpected/10
+                );
+            }
         }
     }
 
-    function testsimulateAmountOutOnSqrtPriceX96CrossTick() public {
-        uint256 _alphaX = 10000000000000000000;
+    ///@notice Test simulateAmountOutOnSqrtPriceX96 on an input quantity that crosses to the next initialized tick in the pool.
+    ///@dev The liquditiy should change in the pool upon crossing a tick, and therefore the exchange rate.
+    function testSimulateAmountOutOnSqrtPriceX96CrossTick(uint112 _alphaX)
+        public
+    {
+        bool run = true;
 
-        (uint160 sqrtPriceX96, int24 tickBefore, , , , , ) = IUniswapV3Pool(
-            0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8
-        ).slot0();
+        {
+            uint112 MAX_INPUT = 1000000000000000000000; //1000 ETH
+            //Input quantities range between 100-1000 ETH to hopefully always have enough to cross a tick
+            if (
+                _alphaX == 0 ||
+                _alphaX > MAX_INPUT ||
+                _alphaX < 100000000000000000000
+            ) {
+                run = false;
+            }
+        }
+        if (run) {
+            //Get the sqrtPriceX96 and the current tick on the v3 pool.
+            (uint160 sqrtPriceX96, int24 tickBefore, , , , , ) = IUniswapV3Pool(
+                usdcWethPoolV3
+            ).slot0();
+            //Get the tick spacing on the pool
+            int24 tickSpacing = IUniswapV3Pool(usdcWethPoolV3).tickSpacing();
+            //Get token0 from the pool
+            address token0 = IUniswapV3Pool(usdcWethPoolV3).token0();
+            //Get the liquidity currently in the pool.
+            uint128 liquidity = IUniswapV3Pool(usdcWethPoolV3).liquidity();
 
-        int24 tickSpacing = IUniswapV3Pool(
-            0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8
-        ).tickSpacing();
-        address token0 = IUniswapV3Pool(
-            0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8
-        ).token0();
-        uint128 liquidity = IUniswapV3Pool(
-            0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8
-        ).liquidity();
-        
+            //Calculate the simulated amountOut to validate accuracy on from ConveyorTickMath
+            uint256 amountOutToValidate = uint256(
+                -conveyorTickMath._simulateAmountOutOnSqrtPriceX96(
+                    token0,
+                    WETH,
+                    usdcWethPoolV3,
+                    _alphaX,
+                    tickSpacing,
+                    liquidity,
+                    500
+                )
+            );
 
-        uint256 amountOutMin = uint256(
-            -conveyorTickMath._simulateAmountOutOnSqrtPriceX96(
-                token0,
+            //Deal some ether to the test contract
+            cheatCodes.deal(address(this), 500000000000 ether);
+
+            //Wrap the Ether
+            address(WETH).call{value: 500000000000 ether}(
+                abi.encodeWithSignature("deposit()")
+            );
+
+            //Transfer the input amount to the SwapRotuer contract to be sent to the pool in the swap callback.
+            IERC20(WETH).transfer(address(swapRouter), _alphaX);
+
+            //Perform the swap on the pool to recalculate if the current tick has been crossed
+            uint256 amountReceived = swapRouter.swapV3(
+                usdcWethPoolV3,
                 WETH,
-                0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8,
+                USDC,
+                500,
                 _alphaX,
-                tickSpacing,
-                liquidity,
-                3000
-            )
-        );
+                amountOutToValidate,
+                address(this),
+                address(swapRouter)
+            );
 
-        //get the token in
-        cheatCodes.deal(address(this), 500000000000 ether);
+            //Get the current tick after the swap to see if a tick has been crossed.
+            (, int24 tickAfter, , , , , ) = IUniswapV3Pool(usdcWethPoolV3)
+                .slot0();
 
-        address(WETH).call{value: 500000000000 ether}(
-            abi.encodeWithSignature("deposit()")
-        );
-
-        IERC20(WETH).transfer(address(limitOrderExecutor), _alphaX);
-
-        //Perform the swap on the pool to recalculate if the current tick has been crossed
-        uint256 amountReceived = limitOrderExecutor.swapV3(
-            0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8,
-            WETH,
-            0x6B175474E89094C44Da98b954EedeAC495271d0F,
-            3000,
-            _alphaX,
-            amountOutMin,
-            address(this),
-            address(limitOrderExecutor)
-        );
-
-        (, int24 tickAfter, , , , , ) = IUniswapV3Pool(
-            0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8
-        ).slot0();
-
-        assertGt(tickAfter, tickBefore);
-        assertEq(amountReceived, amountOutMin);
-      
+            //The tick should have incremented
+            assertGt(tickAfter, tickBefore);
+            //Validate that the amount we received matches out quote.
+            assertLt(amountOutToValidate, amountReceived);
+        }
     }
+
     //Block: 15233771
     function testFromSqrtX96() public {
         address poolAddress = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
         address tokenIn = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
         address tokenOut = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-        
+
         //USDC spot price
         (uint160 sqrtPriceX96, , , , , , ) = IUniswapV3Pool(poolAddress)
-                .slot0();
+            .slot0();
 
-        uint256 priceUSDC=conveyorTickMath._fromSqrtX96(sqrtPriceX96, true, tokenIn, tokenOut);
-        uint256 priceWETH=conveyorTickMath._fromSqrtX96(sqrtPriceX96, false, tokenIn, tokenOut);
+        uint256 priceUSDC = conveyorTickMath._fromSqrtX96(
+            sqrtPriceX96,
+            true,
+            tokenIn,
+            tokenOut
+        );
+        uint256 priceWETH = conveyorTickMath._fromSqrtX96(
+            sqrtPriceX96,
+            false,
+            tokenIn,
+            tokenOut
+        );
         console.log(priceWETH);
         assertEq(194786572365129798010721349067079679, priceUSDC);
         assertEq(594456218574771757927683806232862281426471, priceWETH);
-        
     }
 
     receive() external payable {}
@@ -316,7 +395,7 @@ contract ConveyorTickMathWrapper is ConveyorTickMath {
     }
 }
 
-contract LimitOrderExecutorWrapper is SwapRouter {
+contract SwapRouterWrapper is SwapRouter {
     constructor(
         bytes32[] memory _initBytecodes,
         address[] memory _dexFactories,
