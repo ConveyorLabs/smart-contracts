876664b589bda2ad2311e08ec227e6218c6218a8
Leyton <leytontaylor23@gmail.com>
2022-11-17 15:17:24 -0600

    FeeMath, TickMath, SwapRouter, and Quoter all passing

--- src/test/GasOracle.t.sol
+++ src/test/GasOracle.t.sol
@@ -1,21 +1,19 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.16;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
 
-// import "./utils/test.sol";
-// import "../GasOracle.sol";
+import "./utils/test.sol";
+import "../GasOracle.sol";
 
-// contract GasOracleTest is DSTest {
-//     GasOracle gasOracle;
+contract GasOracleTest is DSTest {
+    GasOracle gasOracle;
 
-//     function setUp() public {
-//         ///@notice This is the fast gas oracle address for Ethereum Mainnet
-//         address aggregatorV3Address = 0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C;
-//         gasOracle = new GasOracle(aggregatorV3Address);
-//     }
+    function setUp() public {
+        ///@notice This is the fast gas oracle address for Ethereum Mainnet
+        address aggregatorV3Address = 0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C;
+        gasOracle = new GasOracle(aggregatorV3Address);
+    }
 
-//     function testGetPrice() public {
-//         gasOracle.getGasPrice();
-//     }
-
-
-// }
+    function testGetPrice() public {
+        gasOracle.getGasPrice();
+    }
+}
