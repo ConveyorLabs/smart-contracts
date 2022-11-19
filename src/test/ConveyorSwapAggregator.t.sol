00aefa3bdbf930af4c73dc88b665862526ebed47
0xKitsune <0xKitsune@protonmail.com>
2022-11-19 15:19:47 -0500

    upadated test

--- src/test/ConveyorSwapAggregator.t.sol
+++ src/test/ConveyorSwapAggregator.t.sol
@@ -3,12 +3,16 @@ pragma solidity 0.8.16;
 
 import "./utils/test.sol";
 import "./utils/Swap.sol";
-import "../csat.sol";
+import "../ConveyorSwapAggregator.sol";
 
 interface CheatCodes {
     function prank(address) external;
 
     function deal(address who, uint256 amount) external;
+
+    function createSelectFork(string calldata, uint256)
+        external
+        returns (uint256);
 }
 
 contract ConveyorSwapAggregatorTest is DSTest {
@@ -24,11 +28,14 @@ contract ConveyorSwapAggregatorTest is DSTest {
         address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
         swapHelper = new Swap(uniV2Addr, WETH);
+        cheatCodes.deal(address(swapHelper), type(uint256).max);
 
         conveyorSwapAggregator = new ConveyorSwapAggregator();
     }
 
-    function testSwap() public {
+    function testSwapUniv2SingleLP() public {
+        // cheatCodes.createSelectFork("mainnet", 16000200);
+
         cheatCodes.deal(address(this), type(uint128).max);
         address tokenIn = 0x34Be5b8C30eE4fDe069DC878989686aBE9884470;
         uint256 amountIn = 1900000000000000000000;
@@ -50,6 +57,49 @@ contract ConveyorSwapAggregatorTest is DSTest {
         ConveyorSwapAggregator.SwapAggregatorMulticall
             memory multicall = ConveyorSwapAggregator.SwapAggregatorMulticall(
                 lp,
+                calls
+            );
+
+        conveyorSwapAggregator.swap(
+            tokenIn,
+            amountIn,
+            tokenOut,
+            amountOutMin,
+            multicall
+        );
+    }
+
+    function testSwapUniv2MultiLP() public {
+        // cheatCodes.createSelectFork("mainnet", 16000233);
+
+        cheatCodes.deal(address(this), type(uint128).max);
+        address tokenIn = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        uint256 amountIn = 825000000;
+        address tokenOut = 0x2e85ae1C47602f7927bCabc2Ff99C40aA222aE15;
+        uint256 amountOutMin = 4948702184313056519683340;
+        address firstLP = 0x9572e4C0c7834F39b5B8dFF95F211d79F92d7F23;
+        address secondLP = 0xdC1D67Bc953Bf67F007243c7DED42d67410a6De5;
+
+        swapHelper.swapEthForTokenWithUniV2(1 ether, tokenIn);
+        IERC20(tokenIn).approve(
+            address(conveyorSwapAggregator),
+            type(uint256).max
+        );
+
+        ConveyorSwapAggregator.Call[]
+            memory calls = new ConveyorSwapAggregator.Call[](2);
+
+        calls[0] = newUniV2Call(firstLP, 0, 680641423375285542, secondLP);
+        calls[1] = newUniV2Call(
+            secondLP,
+            4948702184313056519683340,
+            0,
+            address(this)
+        );
+
+        ConveyorSwapAggregator.SwapAggregatorMulticall
+            memory multicall = ConveyorSwapAggregator.SwapAggregatorMulticall(
+                firstLP,
                 calls
             );
 
