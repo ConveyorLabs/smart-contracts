08f7f3714fc6d24e972dd69fcc634d46d19a68b3
0xKitsune <0xKitsune@protonmail.com>
2022-11-19 15:54:01 -0500

    added v3 callback to executor

--- src/ConveyorSwapAggregator.sol
+++ src/ConveyorSwapAggregator.sol
@@ -75,4 +75,32 @@ contract ConveyorSwapExecutor {
             }
         }
     }
+
+    ///@notice Uniswap V3 callback function called during a swap on a v3 liqudity pool.
+    ///@param amount0Delta - The change in token0 reserves from the swap.
+    ///@param amount1Delta - The change in token1 reserves from the swap.
+    ///@param data - The data packed into the swap.
+    function uniswapV3SwapCallback(
+        int256 amount0Delta,
+        int256 amount1Delta,
+        bytes memory data
+    ) external {
+        ///@notice Decode all of the swap data.
+        (bool _zeroForOne, address tokenIn, address _sender) = abi.decode(
+            data,
+            (bool, address, address)
+        );
+
+        ///@notice Set amountIn to the amountInDelta depending on boolean zeroForOne.
+        uint256 amountIn = _zeroForOne
+            ? uint256(amount0Delta)
+            : uint256(amount1Delta);
+
+        if (!(_sender == address(this))) {
+            ///@notice Transfer the amountIn of tokenIn to the liquidity pool from the sender.
+            IERC20(tokenIn).transferFrom(_sender, msg.sender, amountIn);
+        } else {
+            IERC20(tokenIn).transfer(msg.sender, amountIn);
+        }
+    }
 }
