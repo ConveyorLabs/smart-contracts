14e11095de49dd5e9ee600cd617ce87930b9a0e9
Leyton <leytontaylor23@gmail.com>
2022-11-22 19:35:46 -0600

    Removed duplicate expect revert

--- src/test/LimitOrderBook.t.sol
+++ src/test/LimitOrderBook.t.sol
@@ -243,41 +243,28 @@ contract LimitOrderBookTest is DSTest {
                 assertEq(orderBook.totalOrdersPerAddress(address(this)), 1);
             }
         } catch {
-            if (executionCreditAmount == 0) {
-                cheatCodes.expectRevert(
-                    abi.encodeWithSelector(Errors.InsufficientMsgValue.selector)
-                );
-                (bool reverted, ) = address(orderBook).call(
-                    abi.encodeWithSignature(
-                        "increaseExecutionCredit(bytes32)",
-                        bytes32(0)
-                    )
-                );
-                assertTrue(reverted);
-            } else {
-                cheatCodes.deal(
-                    address(this),
-                    type(uint256).max > address(this).balance
-                        ? type(uint256).max - address(this).balance
-                        : 0
-                );
-                cheatCodes.expectRevert(
-                    abi.encodeWithSelector(
-                        Errors.OrderDoesNotExist.selector,
-                        bytes32(0)
-                    )
-                );
+            cheatCodes.deal(
+                address(this),
+                type(uint256).max > address(this).balance
+                    ? type(uint256).max - address(this).balance
+                    : 0
+            );
+            cheatCodes.expectRevert(
+                abi.encodeWithSelector(
+                    Errors.OrderDoesNotExist.selector,
+                    bytes32(0)
+                )
+            );
 
-                (bool reverted, ) = address(orderBook).call{
-                    value: executionCreditAmount
-                }(
-                    abi.encodeWithSignature(
-                        "increaseExecutionCredit(bytes32)",
-                        bytes32(0)
-                    )
-                );
-                assertTrue(reverted);
-            }
+            (bool reverted, ) = address(orderBook).call{
+                value: executionCreditAmount
+            }(
+                abi.encodeWithSignature(
+                    "increaseExecutionCredit(bytes32)",
+                    bytes32(0)
+                )
+            );
+            assertTrue(reverted);
         }
     }
 
@@ -345,7 +332,6 @@ contract LimitOrderBookTest is DSTest {
                     );
                     assertTrue(reverted2);
                 } else {
-                    // cheatCodes.expectEmit(true, true, false, false);
                     orderBook.decreaseExecutionCredit(
                         orderId,
                         executionCreditAmount
