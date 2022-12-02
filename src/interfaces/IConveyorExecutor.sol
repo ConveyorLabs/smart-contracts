198954eb1575e38fc0ded52ca0751fa7cc49a337
0xKitsune <0xKitsune@protonmail.com>
2022-12-02 15:37:28 -0500

    rolling back to limit order system only with qsp fixes

--- src/interfaces/IConveyorExecutor.sol
+++ src/interfaces/IConveyorExecutor.sol
@@ -2,8 +2,6 @@
 pragma solidity 0.8.16;
 
 import "../LimitOrderBook.sol";
-import "../SandboxLimitOrderBook.sol";
-import "../SandboxLimitOrderRouter.sol";
 
 interface IConveyorExecutor {
     function executeTokenToWethOrders(LimitOrderBook.LimitOrder[] memory orders)
@@ -13,9 +11,4 @@ interface IConveyorExecutor {
     function executeTokenToTokenOrders(
         LimitOrderBook.LimitOrder[] memory orders
     ) external returns (uint256, uint256);
-
-    function executeSandboxLimitOrders(
-        SandboxLimitOrderBook.SandboxLimitOrder[] memory orders,
-        SandboxLimitOrderRouter.SandboxMulticall calldata calls
-    ) external;
 }
