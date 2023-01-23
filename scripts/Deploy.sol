254261f4b80487a865508de836b1292f53889f27
LeytonTaylor <leytontaylor23@gmail.com>
2023-01-23 11:36:32 -0600

    Updated deployment contract

--- scripts/Deploy.sol
+++ scripts/Deploy.sol
@@ -17,26 +17,31 @@ contract Deploy is Script {
     uint256 constant MINIMUM_EXECUTION_CREDITS = 0;
 
     ///@dev Polygon Constructor Constants
-    address constant WMATIC= 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+    address constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
     address constant USDC_POLYGON = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
 
     ///@dev Cfmm Factory addresses & deployment hashes
     address constant SUSHI_POLYGON = 0xc35DADB65012eC5796536bD9864eD8773aBc74C4;
-    bytes32 constant SUSHI_POLYGON_DEPLOYMENT_HASH = 0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303;
+    bytes32 constant SUSHI_POLYGON_DEPLOYMENT_HASH =
+        0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303;
     address constant QUICK_POLYGON = 0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32;
-    bytes32 constant QUICK_POLYGON_DEPLOYMENT_HASH = 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f;
-    address constant UNISWAP_V3_POLYGON = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+    bytes32 constant QUICK_POLYGON_DEPLOYMENT_HASH =
+        0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f;
+    address constant UNISWAP_V3_POLYGON =
+        0x1F98431c8aD98523631AE4a59f267346ea31F984;
     bytes32 constant UNISWAP_V3_POLYGON_DEPLOYMENT_HASH = bytes32(0);
-    
-    ///TODO:
-    function mainnet() public returns (ConveyorExecutor conveyorExecutor, LimitOrderRouter limitOrderRouter, LimitOrderQuoter limitOrderQuoter, ConveyorSwapAggregator conveyorSwapAggregator) {
-        vm.startBroadcast();
-        /// Deploy LimitOrderQuoter
-        // limitOrderQuoter = new LimitOrderQuoter{salt: SALT}(s);
-        vm.stopBroadcast();
-    }
 
-    function polygon() public returns (ConveyorExecutor conveyorExecutor, LimitOrderRouter limitOrderRouter,  SandboxLimitOrderBook sandboxLimitOrderBook, SandboxLimitOrderRouter sandboxLimitOrderRouter, LimitOrderQuoter limitOrderQuoter, ConveyorSwapAggregator conveyorSwapAggregator) {
+    function run()
+        public
+        returns (
+            ConveyorExecutor conveyorExecutor,
+            LimitOrderRouter limitOrderRouter,
+            SandboxLimitOrderBook sandboxLimitOrderBook,
+            SandboxLimitOrderRouter sandboxLimitOrderRouter,
+            LimitOrderQuoter limitOrderQuoter,
+            ConveyorSwapAggregator conveyorSwapAggregator
+        )
+    {
         bytes32[] memory _deploymentByteCodes = new bytes32[](3);
         address[] memory _dexFactories = new address[](3);
         bool[] memory _isUniV2 = new bool[](3);
@@ -57,20 +62,43 @@ contract Deploy is Script {
         /// Deploy LimitOrderQuoter
         limitOrderQuoter = new LimitOrderQuoter{salt: SALT}(WMATIC);
         /// Deploy ConveyorExecutor
-        conveyorExecutor = new ConveyorExecutor{salt: SALT}(WMATIC, USDC_POLYGON, address(limitOrderQuoter),_deploymentByteCodes, _dexFactories, _isUniV2, MINIMUM_EXECUTION_CREDITS);
+        conveyorExecutor = new ConveyorExecutor{salt: SALT}(
+            WMATIC,
+            USDC_POLYGON,
+            address(limitOrderQuoter),
+            _deploymentByteCodes,
+            _dexFactories,
+            _isUniV2,
+            MINIMUM_EXECUTION_CREDITS
+        );
 
         /// Deploy ConveyorSwapAggregator
-        conveyorSwapAggregator = new ConveyorSwapAggregator{salt: SALT}(address(conveyorExecutor));
+        conveyorSwapAggregator = new ConveyorSwapAggregator{salt: SALT}(
+            address(conveyorExecutor)
+        );
 
         /// Deploy LimitOrderRouter
-        limitOrderRouter = new LimitOrderRouter{salt: SALT}(WMATIC, USDC_POLYGON, address(conveyorExecutor), MINIMUM_EXECUTION_CREDITS);
+        limitOrderRouter = new LimitOrderRouter{salt: SALT}(
+            WMATIC,
+            USDC_POLYGON,
+            address(conveyorExecutor),
+            MINIMUM_EXECUTION_CREDITS
+        );
 
         /// Deploy SandboxLimitOrderBook
-        sandboxLimitOrderBook = new SandboxLimitOrderBook{salt: SALT}(address(conveyorExecutor), WMATIC, USDC_POLYGON, MINIMUM_EXECUTION_CREDITS);
+        sandboxLimitOrderBook = new SandboxLimitOrderBook{salt: SALT}(
+            address(conveyorExecutor),
+            WMATIC,
+            USDC_POLYGON,
+            MINIMUM_EXECUTION_CREDITS
+        );
 
         /// Deploy SandboxLimitOrderRouter
-        sandboxLimitOrderRouter = new SandboxLimitOrderRouter{salt: SALT}(address(sandboxLimitOrderBook), address(conveyorExecutor));
-        
+        sandboxLimitOrderRouter = new SandboxLimitOrderRouter{salt: SALT}(
+            address(sandboxLimitOrderBook),
+            address(conveyorExecutor)
+        );
+
         vm.stopBroadcast();
     }
 }
