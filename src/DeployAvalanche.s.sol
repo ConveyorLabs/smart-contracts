ae4ce45cdcf943546abe22b844fa8f564eb43f14
0xOsiris <djosiris@proton.me>
2023-02-12 15:38:07 -0600

    Avalanche deployment script

--- src/DeployAvalanche.s.sol
+++ src/DeployAvalanche.s.sol
@@ -17,14 +17,11 @@ contract Deploy is Script {
     ///@dev Minimum Execution Credits
     uint256 constant MINIMUM_EXECUTION_CREDITS = 1500000000000000;
 
-    address constant WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
-    address constant USDC = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+    address constant WETH = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
+    address constant USDC = 0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E;
 
-    address constant CAMELOT =
-        0x6EcCab422D763aC031210895C81787E87B43A652;
-    address constant KYBERSWAP_V2 = 0x5F1dddbf348aC2fbe22a163e30F99F9ECE3DD50a;
-    address constant SUSHISWAP_V2 = 0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac;
-    address constant UNISWAP_V3 = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+    address constant TRADER_JOE = 0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10;
+    address constant PANGOLIN = 0xefa94DE7a4656D787667C749f7E1223D71E9FD88;
 
     function run()
         public
@@ -37,18 +34,16 @@ contract Deploy is Script {
             ConveyorSwapAggregator conveyorSwapAggregator
         )
     {
-        address[] memory _dexFactories = new address[](4);
-        bool[] memory _isUniV2 = new bool[](4);
+        address[] memory _dexFactories = new address[](2);
+        bool[] memory _isUniV2 = new bool[](2);
 
         _isUniV2[0] = true;
         _isUniV2[1] = true;
-        _isUniV2[2] = true;
-        _isUniV2[3] = false;
+ 
 
-        _dexFactories[0] = CAMELOT;
-        _dexFactories[1] = SUSHISWAP_V2;
-        _dexFactories[2] = KYBERSWAP_V2;
-        _dexFactories[3] = UNISWAP_V3;
+        _dexFactories[0] = TRADER_JOE;
+        _dexFactories[1] = PANGOLIN;
+
 
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
