1f4632dc4a3aaf3aff451b2d9dd3198a92b68c0e
Rory Graman <rorygraman@gmail.com>
2023-02-13 13:43:42 -0600

    updated arbitrum execution script

--- src/DeployArbitrum.s.sol
+++ src/DeployArbitrum.s.sol
@@ -17,9 +17,7 @@ contract Deploy is Script {
     address constant WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
     address constant USDC = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
 
-    address constant CAMELOT =
-        0x6EcCab422D763aC031210895C81787E87B43A652;
-    address constant KYBERSWAP_V2 = 0x5F1dddbf348aC2fbe22a163e30F99F9ECE3DD50a;
+    address constant CAMELOT = 0x6EcCab422D763aC031210895C81787E87B43A652;
     address constant SUSHISWAP_V2 = 0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac;
     address constant UNISWAP_V3 = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
 
@@ -34,18 +32,16 @@ contract Deploy is Script {
             ConveyorSwapAggregator conveyorSwapAggregator
         )
     {
-        address[] memory _dexFactories = new address[](4);
-        bool[] memory _isUniV2 = new bool[](4);
+        address[] memory _dexFactories = new address[](3);
+        bool[] memory _isUniV2 = new bool[](3);
 
         _isUniV2[0] = true;
         _isUniV2[1] = true;
-        _isUniV2[2] = true;
-        _isUniV2[3] = false;
+        _isUniV2[2] = false;
 
         _dexFactories[0] = CAMELOT;
         _dexFactories[1] = SUSHISWAP_V2;
-        _dexFactories[2] = KYBERSWAP_V2;
-        _dexFactories[3] = UNISWAP_V3;
+        _dexFactories[2] = UNISWAP_V3;
 
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
