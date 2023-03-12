294287b6f20c6848369394ea4809a440de9e024c
0xOsiris <djosiris@proton.me>
2023-03-12 16:43:53 -0500

    Configured deployment scripts for all chains

--- src/deploy/DeployOptimismAggregator.s.sol
+++ src/deploy/DeployOptimismAggregator.s.sol
@@ -1,29 +1,24 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.16;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
 
-// import {Script} from "../../lib/forge-std/src/Script.sol";
-// import {ConveyorSwapAggregator} from "../ConveyorSwapAggregator.sol";
+import {Script} from "../../lib/forge-std/src/Script.sol";
+import {ConveyorSwapAggregator} from "../ConveyorSwapAggregator.sol";
 
-// contract Deploy is Script {
-//     ///@dev Fantom Constructor Constants
-//     address constant WETH = 0x4200000000000000000000000000000000000006;
-//     address constant CONVEYOR_SWAP_EXECUTOR =
-//         0x0CF7f3F5f1Ff6580147f959519C4eb888b6F214E;
+contract Deploy is Script {
+    ///@dev Fantom Constructor Constants
+    address constant WETH = 0x4200000000000000000000000000000000000006;
 
-//     function run()
-//         public
-//         returns (ConveyorSwapAggregator conveyorSwapAggregator)
-//     {
-//         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    function run()
+        public
+        returns (ConveyorSwapAggregator conveyorSwapAggregator)
+    {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
-//         vm.startBroadcast(deployerPrivateKey);
-
-//         /// Deploy ConveyorSwapAggregator
-//         conveyorSwapAggregator = new ConveyorSwapAggregator(
-//             WETH,
-//             CONVEYOR_SWAP_EXECUTOR
-//         );
-
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.startBroadcast(deployerPrivateKey);
+        /// Deploy ConveyorSwapAggregator
+        conveyorSwapAggregator = new ConveyorSwapAggregator(
+            WETH
+        );
+        vm.stopBroadcast();
+    }
+}
