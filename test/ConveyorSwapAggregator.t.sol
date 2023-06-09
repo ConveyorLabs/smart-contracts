d23cb1fe289b91c9ef769d8bc2ad2485c158218b
0xOsiris <djosiris@proton.me>
2023-06-09 10:31:40 -0500

    Changed swapExactTokenForEth with referral test to mock real scenario

--- test/ConveyorSwapAggregator.t.sol
+++ test/ConveyorSwapAggregator.t.sol
@@ -260,6 +260,51 @@ contract ConveyorSwapAggregatorTest is DSTest {
         console.log("balance after", address(this).balance);
            
     }
+
+    function testSwapExactTokenForETHWithReferral
+    () public {
+        cheatCodes.rollFork(forkId, 16749139);
+
+        cheatCodes.deal(address(this), type(uint128).max);
+        address tokenIn = 0x34Be5b8C30eE4fDe069DC878989686aBE9884470;
+        uint256 amountIn = 1900000000000000000000;
+        uint256 amountOutMin = 54776144172760093;
+  
+        address lp = 0x9572e4C0c7834F39b5B8dFF95F211d79F92d7F23;
+        uint256 balanceBefore = address(this).balance;
+        swapHelper.swapEthForTokenWithUniV2(1 ether, tokenIn);
+        IERC20(tokenIn).approve(
+            address(conveyorSwapAggregator),
+            type(uint256).max
+        );
+
+        ConveyorSwapAggregator.Call[]
+            memory calls = new ConveyorSwapAggregator.Call[](1);
+
+        calls[0] = newUniV2Call(lp, 0, amountOutMin, address(conveyorSwapAggregator));
+
+        ConveyorSwapAggregator.SwapAggregatorMulticall
+            memory multicall = ConveyorSwapAggregator.SwapAggregatorMulticall(
+                1, //zeroForOne
+                1,  //univ2
+                0, //SwapAggregator
+                300,
+                lp,
+                calls
+            );
+        
+        uint128 protocolFee = 5e16;
+        
+        ConveyorSwapAggregator.ReferralInfo memory referralInfo = ConveyorSwapAggregator.ReferralInfo(
+            address(this),
+            1e16
+        );
+
+        conveyorSwapAggregator.swapExactTokenForEthWithReferral{value: protocolFee}(tokenIn, amountIn, amountOutMin, multicall, referralInfo);
+        console.log("balance before", balanceBefore);
+        console.log("balance after", address(this).balance);
+           
+    }
     receive() external payable {}
 
     function testSwapUniv2MultiLP() public {
