508d001bde48aabab2512a649c65eca6ee351d07
LeytonTaylor <leytontaylor23@gmail.com>
2023-07-27 13:17:23 -0700

    feat: add custom swap fee to uni v2 callback

--- src/UniswapV2Callback.sol
+++ src/UniswapV2Callback.sol
@@ -10,32 +10,45 @@ contract UniswapV2Callback {
     /// @param amount0 - The change in token0 reserves from the swap.
     /// @param amount1 - The change in token1 reserves from the swap.
     /// @param data - The data packed into the swap.
-    function uniswapV2Call(address, uint256 amount0, uint256 amount1, bytes calldata data) external {
+    function uniswapV2Call(
+        address,
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata data
+    ) external {
         ///@notice Decode all of the swap data.
-        (bool _zeroForOne, address _tokenIn, address _sender) = abi.decode(data, (bool, address, address));
+        (bool _zeroForOne, address _tokenIn, uint8 _swapFee) = abi.decode(
+            data,
+            (bool, address, uint8)
+        );
 
         uint256 amountOut = _zeroForOne ? amount1 : amount0;
-        (uint112 reserve0, uint112 reserve1,) = IUniswapV2Pair(msg.sender).getReserves();
+        (uint112 reserve0, uint112 reserve1, ) = IUniswapV2Pair(msg.sender)
+            .getReserves();
 
-        uint256 amountIn = getAmountIn(amountOut, _zeroForOne ? reserve0 : reserve1, _zeroForOne ? reserve1 : reserve0);
+        uint256 amountIn = getAmountIn(
+            amountOut,
+            _zeroForOne ? reserve0 : reserve1,
+            _zeroForOne ? reserve1 : reserve0,
+            _swapFee
+        );
 
-        if (!(_sender == address(this))) {
-            ///@notice Transfer the amountIn of tokenIn to the liquidity pool from the sender.
-            IERC20(_tokenIn).transferFrom(_sender, msg.sender, amountIn);
-        } else {
-            IERC20(_tokenIn).transfer(msg.sender, amountIn);
-        }
+        IERC20(_tokenIn).transfer(msg.sender, amountIn);
     }
 
-    function getAmountIn(uint256 amountOut, uint256 reserveIn, uint256 reserveOut)
-        internal
-        pure
-        returns (uint256 amountIn)
-    {
+    function getAmountIn(
+        uint256 amountOut,
+        uint256 reserveIn,
+        uint256 reserveOut, 
+        uint8 swapFee
+    ) internal pure returns (uint256 amountIn) {
         require(amountOut > 0, "UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT");
-        require(reserveIn > 0 && reserveOut > 0, "UniswapV2Library: INSUFFICIENT_LIQUIDITY");
-        uint256 numerator = reserveIn * amountOut * 1000;
-        uint256 denominator = (reserveOut - amountOut) * 997;
+        require(
+            reserveIn > 0 && reserveOut > 0,
+            "UniswapV2Library: INSUFFICIENT_LIQUIDITY"
+        );
+        uint256 numerator = reserveIn * amountOut * 100000;
+        uint256 denominator = (reserveOut - amountOut) * (100000 - swapFee);
         amountIn = (numerator / denominator) + 1;
     }
 }
