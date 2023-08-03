59be9e73219e843b982a0afdb803d816169bf67b
0xOsiris <djosiris@proton.me>
2023-08-03 13:03:19 -0500

    fix: polygon ref init fee

--- src/deploy/DeployPolygonAggregator.s.sol
+++ src/deploy/DeployPolygonAggregator.s.sol
@@ -14,7 +14,7 @@ contract Deploy is Script {
 
         bytes32 salt = bytes32("0x8fbb158");
         bytes memory creationCode =
-            abi.encodePacked(type(ConveyorRouterV1).creationCode, abi.encode(WMATIC, 184467440737095));
+            abi.encodePacked(type(ConveyorRouterV1).creationCode, abi.encode(WMATIC, 184467440737095520));
 
         vm.startBroadcast();
         conveyorRouterV1 = ICREATE3Factory(0x93FEC2C00BfE902F733B57c5a6CeeD7CD1384AE1).deploy(salt, creationCode);
