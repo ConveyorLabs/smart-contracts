7208472e6446f4f3996c92512499d7ab88bc62e8
0xOsiris <djosiris@proton.me>
2023-08-03 11:57:25 -0500

    feat: bsc callbacks

--- src/callbacks/ConveyorSwapCallbacks.sol
+++ src/callbacks/ConveyorSwapCallbacks.sol
@@ -2,35 +2,29 @@
 pragma solidity ^0.8.19;
 
 import {AlgebraCallback} from "./AlgebraCallback.sol";
-import {ConvergenceXCallback} from "./ConvergenceXCallback.sol";
-import {DXSwapCallback} from "./DXSwapCallback.sol";
-import {ElkSwapCallback} from "./ElkSwapCallback.sol";
-import {JetSwapCallback} from "./JetSwapCallback.sol";
-import {LinkSwapCallback} from "./LinkSwapCallback.sol";
-import {MeerkatCallback} from "./MeerkatCallback.sol";
 import {PancakeV2Callback} from "./PancakeV2Callback.sol";
 import {PancakeV3Callback} from "./PancakeV3Callback.sol";
-import {SakeSwapCallback} from "./SakeSwapCallback.sol";
-import {UniFiCallback} from "./UniFiCallback.sol";
 import {UniswapV2Callback} from "./UniswapV2Callback.sol";
 import {UniswapV3Callback} from "./UniswapV3Callback.sol";
-import {VerseCallback} from "./VerseCallback.sol";
-import {WaultSwapCallback} from "./WaultSwapCallback.sol";
+import {BiswapCallback} from "./BiswapCallback.sol";
+import {BabySwapCallback} from "./BabySwapCallback.sol";
+import {TraderJoeCallback} from "./TraderJoeCallback.sol";
+import {MdexSwapCallback} from "./MdexSwapCallback.sol";
+import {BabyDogeCallback} from "./BabyDogeCallback.sol";
+import {NomiswapCallback} from "./NomiswapCallback.sol";
+import {CafeSwapCallback} from "./CafeSwapCallback.sol";
 
 contract ConveyorSwapCallbacks is
     AlgebraCallback,
-    ConvergenceXCallback,
-    DXSwapCallback,
-    ElkSwapCallback,
-    JetSwapCallback,
-    LinkSwapCallback,
-    MeerkatCallback,
     PancakeV2Callback,
     PancakeV3Callback,
-    SakeSwapCallback,
-    UniFiCallback,
     UniswapV2Callback,
     UniswapV3Callback,
-    VerseCallback,
-    WaultSwapCallback
+    BiswapCallback,
+    BabySwapCallback,
+    TraderJoeCallback,
+    MdexSwapCallback,
+    BabyDogeCallback,
+    NomiswapCallback,
+    CafeSwapCallback
 {}
