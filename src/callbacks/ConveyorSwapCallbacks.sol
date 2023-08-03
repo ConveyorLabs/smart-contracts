e3fde91e7dc4675cb13828b50673474c553eac53
0xOsiris <djosiris@proton.me>
2023-08-03 12:23:35 -0500

    feat: polygon callbacks

--- src/callbacks/ConveyorSwapCallbacks.sol
+++ src/callbacks/ConveyorSwapCallbacks.sol
@@ -2,29 +2,28 @@
 pragma solidity ^0.8.19;
 
 import {AlgebraCallback} from "./AlgebraCallback.sol";
-import {PancakeV2Callback} from "./PancakeV2Callback.sol";
-import {PancakeV3Callback} from "./PancakeV3Callback.sol";
 import {UniswapV2Callback} from "./UniswapV2Callback.sol";
 import {UniswapV3Callback} from "./UniswapV3Callback.sol";
-import {BiswapCallback} from "./BiswapCallback.sol";
-import {BabySwapCallback} from "./BabySwapCallback.sol";
-import {TraderJoeCallback} from "./TraderJoeCallback.sol";
-import {MdexSwapCallback} from "./MdexSwapCallback.sol";
-import {BabyDogeCallback} from "./BabyDogeCallback.sol";
-import {NomiswapCallback} from "./NomiswapCallback.sol";
-import {CafeSwapCallback} from "./CafeSwapCallback.sol";
+import {ApeSwapCallback} from "./ApeSwapCallback.sol";
+import {MeerkatCallback} from "./MeerkatCallback.sol";
+import {KyberSwapV3Callback} from "./KyberSwapV3Callback.sol";
+import {WaultSwapCallback} from "./WaultSwapCallback.sol";
+import {JetSwapCallback} from "./JetSwapCallback.sol";
+import {ElkSwapCallback} from "./ElkSwapCallback.sol";
+import {DystopiaCallback} from "./DystopiaCallback.sol";
+import {UniFiCallback} from "./UniFiCallback.sol";
+import {VerseCallback} from "./VerseCallback.sol";
 
 contract ConveyorSwapCallbacks is
     AlgebraCallback,
-    PancakeV2Callback,
-    PancakeV3Callback,
+    ApeSwapCallback,
     UniswapV2Callback,
     UniswapV3Callback,
-    BiswapCallback,
-    BabySwapCallback,
-    TraderJoeCallback,
-    MdexSwapCallback,
-    BabyDogeCallback,
-    NomiswapCallback,
-    CafeSwapCallback
+    MeerkatCallback,
+    KyberSwapV3Callback,
+    WaultSwapCallback,
+    JetSwapCallback,
+    ElkSwapCallback,
+    DystopiaCallback,
+    UniFiCallback
 {}
---
+++ src/callbacks/KyberSwapV3Callback.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.19;
+
+import "../../lib/interfaces/token/IERC20.sol";
+
+contract KyberSwapV3Callback {
+    ///@notice KyberSwap callback function called during a swap on a v3 liqudity pool.
+    ///@param amount0Delta - The change in token0 reserves from the swap.
+    ///@param amount1Delta - The change in token1 reserves from the swap.
+    ///@param data - The data packed into the swap.
+    function swapCallback(int256 amount0Delta, int256 amount1Delta, bytes calldata data) external {
+        ///@notice Decode all of the swap data.
+        (bool _zeroForOne, address _tokenIn, address _sender) = abi.decode(data, (bool, address, address));
+
+        ///@notice Set amountIn to the amountInDelta depending on boolean zeroForOne.
+        uint256 amountIn = _zeroForOne ? uint256(amount0Delta) : uint256(amount1Delta);
+
+        if (!(_sender == address(this))) {
+            ///@notice Transfer the amountIn of tokenIn to the liquidity pool from the sender.
+            IERC20(_tokenIn).transferFrom(_sender, msg.sender, amountIn);
+        } else {
+            IERC20(_tokenIn).transfer(msg.sender, amountIn);
+        }
+    }
+}
