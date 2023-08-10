5903fb6617d61801468dd8fe595c0893d360df56
LeytonTaylor <leytontaylor23@gmail.com>
2023-08-10 14:27:20 -0700

    fix: duplicate callback

--- src/callbacks/ConveyorSwapCallbacks.sol
+++ src/callbacks/ConveyorSwapCallbacks.sol
@@ -5,7 +5,6 @@ import {UniswapV3Callback} from "./UniswapV3Callback.sol";
 import {AlgebraCallback} from "./AlgebraCallback.sol";
 import {UniswapV2Callback} from "./UniswapV2Callback.sol";
 import {TraderJoeCallback} from "./TraderJoeCallback.sol";
-import {UniswapV2Callback} from "./UniswapV2Callback.sol";
 import {ZyberSwapElasticCallback} from "./ZyberSwapElasticCallback.sol";
 import {ZyberSwapCallback} from "./ZyberSwapCallback.sol";
 import {ArbDexCallback} from "./ArbDexCallback.sol";
