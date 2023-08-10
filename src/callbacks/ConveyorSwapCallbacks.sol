945b05a5d2722fd573ab1712083ad2dba9a48b5d
LeytonTaylor <leytontaylor23@gmail.com>
2023-08-10 14:13:07 -0700

    feat: arbitrum deployment config

--- src/callbacks/ConveyorSwapCallbacks.sol
+++ src/callbacks/ConveyorSwapCallbacks.sol
@@ -1,28 +1,23 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.19;
 
-import {PancakeV2Callback} from "./PancakeV2Callback.sol";
-import {PancakeV3Callback} from "./PancakeV3Callback.sol";
-import {UniswapV2Callback} from "./UniswapV2Callback.sol";
 import {UniswapV3Callback} from "./UniswapV3Callback.sol";
-import {ConvergenceXCallback} from "./ConvergenceXCallback.sol";
-import {UniFiCallback} from "./UniFiCallback.sol";
-import {VerseCallback} from "./VerseCallback.sol";
-import {ApeSwapCallback} from "./ApeSwapCallback.sol";
-import {LinkSwapCallback} from "./LinkSwapCallback.sol";
-import {SakeSwapCallback} from "./SakeSwapCallback.sol";
-import {DefiSwapCallback} from "./DefiSwapCallback.sol";
+import {AlgebraCallback} from "./AlgebraCallback.sol";
+import {UniswapV2Callback} from "./UniswapV2Callback.sol";
+import {TraderJoeCallback} from "./TraderJoeCallback.sol";
+import {UniswapV2Callback} from "./UniswapV2Callback.sol";
+import {ZyberSwapElasticCallback} from "./ZyberSwapElasticCallback.sol";
+import {ZyberSwapCallback} from "./ZyberSwapCallback.sol";
+import {ArbDexCallback} from "./ArbDexCallback.sol";
+import {ArbSwapCallback} from "./ArbSwapCallback.sol";
 
 contract ConveyorSwapCallbacks is
-    PancakeV2Callback,
-    PancakeV3Callback,
-    UniswapV2Callback,
     UniswapV3Callback,
-    ConvergenceXCallback,
-    UniFiCallback,
-    VerseCallback,
-    ApeSwapCallback,
-    LinkSwapCallback,
-    SakeSwapCallback,
-    DefiSwapCallback
+    AlgebraCallback,
+    TraderJoeCallback,
+    UniswapV2Callback,
+    ZyberSwapElasticCallback,
+    ZyberSwapCallback,
+    ArbDexCallback,
+    ArbSwapCallback
 {}
--- src/callbacks/ZyberSwapElasticCallback.sol
+++ src/callbacks/ZyberSwapElasticCallback.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.8.19;
 
 import "../../lib/interfaces/token/IERC20.sol";
 
-contract AlgebraCallback {
-    ///@notice Algebra callback function called during a swap on a algebra liqudity pool.
-    ///@param amount0 - The change in token0 reserves from the swap.
-    ///@param amount1 - The change in token1 reserves from the swap.
+contract ZyberSwapElasticCallback {
+    ///@notice ZyberSwap Elastic callback function called during a swap on a v3 liqudity pool.
+    ///@param deltaQty0 - The change in token0 reserves from the swap.
+    ///@param deltaQty1 - The change in token1 reserves from the swap.
     ///@param data - The data packed into the swap.
-    function algebraSwapCallback(int256 amount0, int256 amount1, bytes calldata data) external {
+    function swapCallback(int256 deltaQty0, int256 deltaQty1, bytes calldata data) external {
         ///@notice Decode all of the swap data.
         (bool _zeroForOne, address _tokenIn, address _sender) = abi.decode(data, (bool, address, address));
 
         ///@notice Set amountIn to the amountInDelta depending on boolean zeroForOne.
-        uint256 amountIn = _zeroForOne ? uint256(amount0) : uint256(amount1);
+        uint256 amountIn = _zeroForOne ? uint256(deltaQty0) : uint256(deltaQty1);
 
         if (!(_sender == address(this))) {
             ///@notice Transfer the amountIn of tokenIn to the liquidity pool from the sender.
