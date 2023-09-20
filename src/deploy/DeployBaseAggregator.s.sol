55fbe027da4dba5deb6a2d1aab343d0ca19a7b40
0xOsiris <djosiris@proton.me>
2023-09-20 13:19:42 -0500

    add base deployment script

---
+++ src/deploy/DeployBaseAggregator.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.21;
+
+import {Script} from "../../lib/forge-std/src/Script.sol";
+import {ConveyorRouterV1} from "../ConveyorRouterV1.sol";
+import {ICREATE3Factory} from "../../lib/create3-factory/src/ICREATE3Factory.sol";
+
+contract Deploy is Script {
+    ///@dev Base Constructor Constants
+    address constant WETH = 0x4200000000000000000000000000000000000006;
+
+    function run() public returns (address conveyorRouterV1) {
+        bytes32 salt = bytes32("0x8fbb158");
+        bytes memory creationCode = abi.encodePacked(type(ConveyorRouterV1).creationCode, abi.encode(WETH));
+
+        vm.startBroadcast();
+        conveyorRouterV1 = ICREATE3Factory(0x93FEC2C00BfE902F733B57c5a6CeeD7CD1384AE1).deploy(salt, creationCode);
+        vm.stopBroadcast();
+    }
+}
