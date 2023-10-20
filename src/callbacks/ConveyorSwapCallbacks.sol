35f322b3770fb59a0ec1bd31ce666f004fc97c2d
LeytonTaylor <leytontaylor23@gmail.com>
2023-10-20 12:51:53 -0700

    update callbacks

--- src/callbacks/ConveyorSwapCallbacks.sol
+++ src/callbacks/ConveyorSwapCallbacks.sol
@@ -1,32 +1,29 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity =0.8.21;
 
-import {PancakeV2Callback} from "./PancakeV2Callback.sol";
-import {PancakeV3Callback} from "./PancakeV3Callback.sol";
+import {AlgebraCallback} from "./AlgebraCallback.sol";
 import {UniswapV2Callback} from "./UniswapV2Callback.sol";
 import {UniswapV3Callback} from "./UniswapV3Callback.sol";
-import {ConvergenceXCallback} from "./ConvergenceXCallback.sol";
+import {ApeSwapCallback} from "./ApeSwapCallback.sol";
+import {MeerkatCallback} from "./MeerkatCallback.sol";
+import {KyberSwapV3Callback} from "./KyberSwapV3Callback.sol";
+import {WaultSwapCallback} from "./WaultSwapCallback.sol";
+import {JetSwapCallback} from "./JetSwapCallback.sol";
+import {ElkSwapCallback} from "./ElkSwapCallback.sol";
+import {DystopiaCallback} from "./DystopiaCallback.sol";
 import {UniFiCallback} from "./UniFiCallback.sol";
 import {VerseCallback} from "./VerseCallback.sol";
-import {ApeSwapCallback} from "./ApeSwapCallback.sol";
-import {LinkSwapCallback} from "./LinkSwapCallback.sol";
-import {SakeSwapCallback} from "./SakeSwapCallback.sol";
-import {DefiSwapCallback} from "./DefiSwapCallback.sol";
-import {KyberSwapV3Callback} from "./KyberSwapV3Callback.sol";
-import {AlgebraCallback} from "./AlgebraCallback.sol";
 
 contract ConveyorSwapCallbacks is
-    PancakeV2Callback,
-    PancakeV3Callback,
+    AlgebraCallback,
+    ApeSwapCallback,
     UniswapV2Callback,
     UniswapV3Callback,
-    ConvergenceXCallback,
-    UniFiCallback,
-    VerseCallback,
-    ApeSwapCallback,
-    LinkSwapCallback,
-    SakeSwapCallback,
-    DefiSwapCallback,
+    MeerkatCallback,
     KyberSwapV3Callback,
-    AlgebraCallback
+    WaultSwapCallback,
+    JetSwapCallback,
+    ElkSwapCallback,
+    DystopiaCallback,
+    UniFiCallback
 {}
