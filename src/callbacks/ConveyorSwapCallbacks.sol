91008f148aad2cdd7ce1ed902af286afc2f4236f
LeytonTaylor <leytontaylor23@gmail.com>
2023-10-20 12:56:28 -0700

    update callbacks

--- src/callbacks/ConveyorSwapCallbacks.sol
+++ src/callbacks/ConveyorSwapCallbacks.sol
@@ -4,30 +4,19 @@ pragma solidity =0.8.21;
 import {UniswapV3Callback} from "./UniswapV3Callback.sol";
 import {AlgebraCallback} from "./AlgebraCallback.sol";
 import {UniswapV2Callback} from "./UniswapV2Callback.sol";
-import {UniswapV3Callback} from "./UniswapV3Callback.sol";
-import {ConvergenceXCallback} from "./ConvergenceXCallback.sol";
-import {UniFiCallback} from "./UniFiCallback.sol";
-import {VerseCallback} from "./VerseCallback.sol";
-import {ApeSwapCallback} from "./ApeSwapCallback.sol";
-import {LinkSwapCallback} from "./LinkSwapCallback.sol";
-import {SakeSwapCallback} from "./SakeSwapCallback.sol";
-import {DefiSwapCallback} from "./DefiSwapCallback.sol";
-import {KyberSwapV3Callback} from "./KyberSwapV3Callback.sol";
-import {AlgebraCallback} from "./AlgebraCallback.sol";
+import {TraderJoeCallback} from "./TraderJoeCallback.sol";
+import {ZyberSwapElasticCallback} from "./ZyberSwapElasticCallback.sol";
+import {ZyberSwapCallback} from "./ZyberSwapCallback.sol";
+import {ArbDexCallback} from "./ArbDexCallback.sol";
+import {ArbSwapCallback} from "./ArbSwapCallback.sol";
 
 contract ConveyorSwapCallbacks is
     UniswapV3Callback,
     AlgebraCallback,
     TraderJoeCallback,
     UniswapV2Callback,
-    UniswapV3Callback,
-    ConvergenceXCallback,
-    UniFiCallback,
-    VerseCallback,
-    ApeSwapCallback,
-    LinkSwapCallback,
-    SakeSwapCallback,
-    DefiSwapCallback,
-    KyberSwapV3Callback,
-    AlgebraCallback
+    ZyberSwapElasticCallback,
+    ZyberSwapCallback,
+    ArbDexCallback,
+    ArbSwapCallback
 {}
