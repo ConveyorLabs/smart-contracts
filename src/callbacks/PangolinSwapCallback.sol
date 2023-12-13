ec6e7d5ef7eb69990c253bd75a2865f02076c25e
0xOsiris <djosiris@proton.me>
2023-12-13 09:26:50 -0600

    feat: Pangolin swap callback

---
+++ src/callbacks/PangolinSwapCallback.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.21;
+
+contract PangolinSwapCallback {
+    bytes4 private constant _UNISWAP_PAIR_RESERVES_CALL_SELECTOR = 0x0902f1ac; // getReserves()
+
+    /// @notice Pangolin swap callback
+    /// @param amount0 - The change in token0 reserves from the swap.
+    /// @param amount1 - The change in token1 reserves from the swap.
+    /// @param data - The data packed into the swap.
+    function pangolinCall(address, uint256 amount0, uint256 amount1, bytes calldata data) external {
+        assembly {
+            // Start at fmp
+            let freeMemoryPointer := mload(0x40)
+            let token := calldataload(data.offset)
+            let fee := calldataload(add(data.offset, 0x20))
+            mstore(freeMemoryPointer, _UNISWAP_PAIR_RESERVES_CALL_SELECTOR) // getReserves()
+            if iszero(staticcall(gas(), caller(), freeMemoryPointer, 0x4, freeMemoryPointer, 0x40)) {
+                // Revert if the call failed.
+                revert(0, 0)
+            }
+
+            if iszero(eq(returndatasize(), 0x60)) {
+                // Revert if the return data size doesn't match the expected size.
+                revert(0, 0)
+            }
+
+            let reserve0 := mload(freeMemoryPointer)
+            let reserve1 := mload(add(freeMemoryPointer, 0x20))
+
+            mstore(freeMemoryPointer, 0xa9059cbb00000000000000000000000000000000000000000000000000000000)
+            mstore(add(freeMemoryPointer, 4), and(caller(), 0xffffffffffffffffffffffffffffffffffffffff)) // Append and mask the "to" argument.
+            switch eq(amount1, 0)
+            case 0 {
+                mstore(
+                    add(freeMemoryPointer, 36),
+                    add(div(mul(mul(reserve0, amount1), 100000), mul(sub(reserve1, amount1), sub(100000, fee))), 1)
+                )
+            }
+            default {
+                mstore(
+                    add(freeMemoryPointer, 36),
+                    add(div(mul(mul(reserve1, amount0), 100000), mul(sub(reserve0, amount0), sub(100000, fee))), 1)
+                )
+            }
+            if iszero(
+                and(
+                    or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                    call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
+                )
+            ) {
+                // Revert if the call failed.
+                revert(0, 0)
+            }
+        }
+    }
+}
--- src/deploy/DeployAvalancheAggregator.s.sol
+++ src/deploy/DeployAvalancheAggregator.s.sol
@@ -3,19 +3,18 @@ pragma solidity =0.8.21;
 
 import {Script} from "../../lib/forge-std/src/Script.sol";
 import {ConveyorRouterV1} from "../ConveyorRouterV1.sol";
+import {ICREATE3Factory} from "../../lib/create3-factory/src/ICREATE3Factory.sol";
 
 contract Deploy is Script {
     ///@dev Avalanche Constructor Constants
-    address constant WAVAX = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
+    address constant WETH = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
 
-    function run() public returns (ConveyorRouterV1 conveyorRouterV1) {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    function run() public returns (address conveyorRouterV1) {
+        bytes32 salt = bytes32("0x8fbb158");
+        bytes memory creationCode = abi.encodePacked(type(ConveyorRouterV1).creationCode, abi.encode(WETH));
 
-        vm.startBroadcast(deployerPrivateKey);
-
-        /// Deploy ConveyorRouterV1
-        conveyorRouterV1 = new ConveyorRouterV1(WAVAX);
-
+        vm.startBroadcast();
+        conveyorRouterV1 = ICREATE3Factory(0x93FEC2C00BfE902F733B57c5a6CeeD7CD1384AE1).deploy(salt, creationCode);
         vm.stopBroadcast();
     }
 }
